30c666f7b79b7a1b92d5f64b70248047e5a91a1a
Paul Alfille <paul.alfille@gmail.com>
2004-02-18 03:20:50 +0000

    release script

---
+++ release.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl -w
+#
+my $prefix = "/snowmass/home/owfs/" ;
+my $pwd = `pwd 2> /dev/null` ;
+chomp($pwd) ;
+
+sub GetVersion {
+  while (1) {
+    printf "Major:" ;
+    my $major = <> ;
+    printf "Minor:" ;
+    my $minor = <> ;
+    printf "Patch level:" ;
+    my $patch = <> ;
+    my @v = map {chomp;$_} ($major,$minor,$patch) ;
+    printf "Is ".join("_",("RELEASE",@v))." correct? (y/n)" ;
+    my $ans = <> ;
+    redo unless $ans =~ m/^[yY]/ ;
+    return @v ;
+  }
+} ;
+
+sub ConfigureAC {
+  my ($maj,$min,$pat) = @_ ;
+  printf "RELEASE -- updating configure.ac\n" ;
+  `/usr/bin/perl -i.old -pe 's\{VERSION_MAJOR=.*?\$\}\{VERSION_MAJOR=$maj}' configure.ac` ;
+  `/usr/bin/perl -i.old -pe 's\{VERSION_MINOR=.*?\$\}\{VERSION_MINOR=$min}' configure.ac` ;
+  `/usr/bin/perl -i.old -pe 's\{PATCHLEVEL=.*?\$\}\{PATCHLEVEL=$pat}' configure.ac` ;
+}
+
+sub PerModule {
+  my ($mod,@v) = @_ ;
+  # pushd
+  chdir $prefix."/".$mod or die "Cannot change to dir $prefix from $pwd" ;
+
+  printf "RELEASE -- cvs branch tagging ".join("_",("RELEASE",@v,"RC"))."\n" ;
+  system("cvs","tag","-b",join("_",("RELEASE",@v,"RC")) ) ;
+  printf "RELEASE -- cvs updating ".join("_",("RELEASE",@v,"RC"))."\n" ;
+  system("cvs","update","-r",join("_",("RELEASE",@v,"RC")) ) ;
+  printf "RELEASE -- bootstrapping\n" ;
+  `./bootstrap` ;
+
+  # change the configure.ac and re-bootstrap
+  ConfigureAC(@v) ;
+
+  # make process
+  printf "RELEASE -- make clean\n" ;
+  system("make","clean") ;
+  printf "RELEASE -- make distclean\n" ;
+  system("make","distclean") ;
+  printf "RELEASE -- make tarball\n" ;
+  system("make","dist") ;
+  printf "RELEASE -- make rpm\n" ;
+  system("make","rpm") ;
+
+  printf $mod."-".$v[0].".".$v[1]."p".$v[2].".tar.gz = ".system("ls","*.tar.gz") ;
+
+  printf "RELEASE -- cvs branch tagging ".join("_",("RELEASE",@v))."\n" ;
+  #system("cvs","tag","-b",join("_",("RELEASE",@v,"RC")) ) ;
+
+  #popd
+  chdir $pwd or die "Cannot change back to dir $pwd from $prefix: $!" ;
+
+} ;
+
+# pushd
+chdir $prefix or die "Cannot change to dir $prefix from $pwd" ;
+
+printf "OWFS releaser\n" ;
+my @v = GetVersion() ;
+
+printf "RELEASE -- release name\n" ;
+printf join("_",("RELEASE",@v,"RC"))."\n" ;
+PerModule("owlib",@v) ;
