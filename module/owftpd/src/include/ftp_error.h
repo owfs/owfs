e5e1a59575a739c33d5663db5bee19064994cebf
Paul Alfille <paul.alfille@gmail.com>
2004-10-10 20:26:32 +0000

    *** empty log message ***

--- module/owftpd/src/include/ftp_error.h
+++ module/owftpd/src/include/ftp_error.h
@@ -8,11 +8,11 @@
 typedef struct {
     int error_code;
     char desc[128];
-} error_t;
+} error_code_t;
 
 /* methods */
-void error_init(error_t *err, int error_code, const char *desc_fmt, ...);
-int error_get_error_code(const error_t *err);
-const char *error_get_desc(const error_t *err);
+void error_init(error_code_t *err, int error_code, const char *desc_fmt, ...);
+int error_get_error_code(const error_code_t *err);
+const char *error_get_desc(const error_code_t *err);
 
 #endif /* ERROR_H */
