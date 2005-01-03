e4678cb7e98557245caf897377e11cbd07a3f29e
Serg Oskin <serg@oskin.ru>
2005-01-03 21:49:21 +0000

    Add "reset" - power-on reset control.

--- module/owlib/src/c/ow_2408.c
+++ module/owlib/src/c/ow_2408.c
@@ -55,6 +55,8 @@ uWRITE_FUNCTION( FS_w_pio ) ;
 uWRITE_FUNCTION( FS_w_latch ) ;
  uREAD_FUNCTION( FS_r_s_alarm ) ;
 uWRITE_FUNCTION( FS_w_s_alarm ) ;
+ yREAD_FUNCTION( FS_r_reset ) ;
+yWRITE_FUNCTION( FS_w_reset ) ;
 
 /* ------- Structures ----------- */
 
@@ -67,6 +69,7 @@ struct filetype DS2408[] = {
     {"latch"     ,     1,  &A2408,  ft_bitfield, ft_volatile, {u:FS_r_latch}  , {u:FS_w_latch},  NULL, } ,
     {"strobe"    ,     1,  NULL,    ft_yesno   , ft_stable  , {y:FS_r_strobe} , {y:FS_w_strobe}, NULL, } ,
     {"set_alarm" ,     9,  NULL,    ft_unsigned, ft_stable  , {u:FS_r_s_alarm}, {u:FS_w_s_alarm},NULL, } ,
+    {"reset"     ,     1,  NULL,    ft_yesno   , ft_stable  , {y:FS_r_reset}  , {y:FS_w_reset},  NULL, } ,
 } ;
 DeviceEntry( 29, DS2408 )
 
@@ -168,6 +171,20 @@ static int FS_w_s_alarm(const unsigned int * u , const struct parsedname * pn) {
     data[2] = (u[0] / 100000000 % 10) & 0x03 ;
     if ( OW_w_s_alarm(data,pn) ) return -EINVAL ;
     return 0 ;
+}
+
+static int FS_r_reset(int * y , const struct parsedname * pn) {
+    unsigned char data[8] ;
+    if ( OW_r_reg(data,pn) ) return -EINVAL ;
+    *y = UT_getbit(&data[5],3) ;
+    return 0 ;
+}
+
+static int FS_w_reset(const int * y, const struct parsedname * pn) {
+    unsigned char data[8] ;
+    if ( OW_r_reg(data,pn) ) return -EINVAL ;
+    UT_setbit( &data[5], 3, y[0] ) ;
+    return OW_w_control( data[5] , pn ) ? -EINVAL : 0 ;
 }
 
 /* Read 6 bytes --
