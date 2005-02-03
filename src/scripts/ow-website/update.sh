9221c482dc90f2a2f769f2d8acbfb2788638b0eb
Peter Kropf <pkropf@gmail.com>
2005-02-03 05:09:32 +0000

    file mode is now +x

---
+++ src/scripts/ow-website/update.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#
+# $Id$
+#
+# This script updates the owfs web site from the contents of the
+# ow-website CVS module. Its run on a regular basis from a crontab
+# entry on shell.sf.net.
+#
+# Crontab entry
+# 3 * * * *      /home/groups/o/ow/owfs/owfs/src/scripts/ow-website/update.sh >> /home/groups/o/ow/owfs/web_update.log 2>&1
+
+
+date
+cd /home/groups/o/ow/owfs/htdocs
+pwd
+cvs -q update
+cd /home/groups/o/ow/owfs/owfs
+pwd
+cvs -q update
