754812c83a62b5f11be8300cfa2d1938326da318
Christian Magnusson <mag@mag.cx>
2005-03-04 10:19:52 +0000

    Fix compiler warning

--- module/owlib/src/c/ow_tree.c
+++ module/owlib/src/c/ow_tree.c
@@ -44,13 +44,14 @@ printf("\n");
 }
 
 static void free_node(void *nodep) {
+  (void) nodep ;
   /* nothing to free */
   return;
 }
 
 void DeviceDestroy( void ) {
-  int i;
-  for(i=0; i<sizeof(Tree)/sizeof(void *); i++) {
+  unsigned int i;
+  for(i=0; i < (sizeof(Tree)/sizeof(void *)); i++) {
     if(Tree[i]) {
       /* pn_structure is just a duplicate of pn_real */
       if(i != pn_structure) tdestroy(Tree[i], free_node);
