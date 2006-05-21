96627fd3d910a9260428c186e42ae544ef91ebc6
Paul Alfille <paul.alfille@gmail.com>
2006-05-21 18:39:07 +0000

    format changes -- no functional changes

--- module/owftpd/src/c/telnet_session.c
+++ module/owftpd/src/c/telnet_session.c
@@ -59,8 +59,7 @@ static void add_outgoing_char(struct telnet_session_s *t, int c);
 static int max_input_read(struct telnet_session_s *t);
 
 /* initialize a telnet session */
-void telnet_session_init(struct telnet_session_s *t, int in, int out)
-{
+void telnet_session_init(struct telnet_session_s *t, int in, int out) {
     daemon_assert(t != NULL);
     daemon_assert(in >= 0);
     daemon_assert(out >= 0);
@@ -104,12 +103,11 @@ int telnet_session_print(struct telnet_session_s *t, const char *s)
             daemon_assert(invariant(t));
             return 0;
         }
-        while ((amt_printed < len) && (t->out_buflen < BUF_LEN))
-	{
+        while ((amt_printed < len) && (t->out_buflen < BUF_LEN)) {
             daemon_assert(s[amt_printed] != '\0');
             add_outgoing_char(t, s[amt_printed]);
             amt_printed++;
-	}
+        }
         process_data(t, 1);
     } while (amt_printed < len);
 
@@ -119,7 +117,7 @@ int telnet_session_print(struct telnet_session_s *t, const char *s)
             return 0;
         }
         process_data(t, 1);
-    } 
+    }
 
     daemon_assert(invariant(t));
     return 1;
@@ -151,27 +149,27 @@ int telnet_session_readln(struct telnet_session_s *t, char *buf, int buflen)
     for (;;) {
         if ((t->in_errno != 0) || (t->in_eof != 0)) {
             daemon_assert(invariant(t));
-	    return 0;
-	}
-	while (t->in_buflen > 0) {
-	    if (amt_read == buflen-1) {
-	        buf[amt_read] = '\0';
+            return 0;
+        }
+        while (t->in_buflen > 0) {
+            if (amt_read == buflen-1) {
+                buf[amt_read] = '\0';
                 daemon_assert(invariant(t));
-		return 1;
-	    }
+                return 1;
+            }
             daemon_assert(amt_read >= 0);
             daemon_assert(amt_read < buflen);
-	    buf[amt_read] = use_incoming_char(t);
-	    if (buf[amt_read] == '\012') {
+            buf[amt_read] = use_incoming_char(t);
+            if (buf[amt_read] == '\012') {
                 daemon_assert(amt_read+1 >= 0);
                 daemon_assert(amt_read+1 < buflen);
-	        buf[amt_read+1] = '\0';
+                buf[amt_read+1] = '\0';
                 daemon_assert(invariant(t));
-	        return 1;
-	    }
+                return 1;
+            }
             amt_read++;
         }
-	process_data(t, 1);
+        process_data(t, 1);
     }
 }
 
@@ -211,7 +209,7 @@ static void process_data(struct telnet_session_s *t, int wait_flag)
     /* only check for input if we can accept input */
     if ((t->in_errno == 0) && 
         (t->in_eof == 0) && 
-	(max_input_read(t) > 0)) 
+        (max_input_read(t) > 0))
     {
         FD_SET(t->in_fd, &readfds);
         FD_SET(t->in_fd, &exceptfds);
@@ -227,21 +225,20 @@ static void process_data(struct telnet_session_s *t, int wait_flag)
     if (select(FD_SETSIZE, &readfds, &writefds, &exceptfds, tv) > 0) {
 
         if (FD_ISSET(t->in_fd, &exceptfds)) {
-	    t->in_eof = 1;
-	} else if (FD_ISSET(t->in_fd, &readfds)) {
-	    read_incoming_data(t);
-	}
+            t->in_eof = 1;
+        } else if (FD_ISSET(t->in_fd, &readfds)) {
+            read_incoming_data(t);
+        }
 
         if (FD_ISSET(t->out_fd, &exceptfds)) {
-	    t->out_eof = 1;
-	} else if (FD_ISSET(t->out_fd, &writefds)) {
-	    write_outgoing_data(t);
+            t->out_eof = 1;
+        } else if (FD_ISSET(t->out_fd, &writefds)) {
+            write_outgoing_data(t);
         }
     }
 }
 
-static void read_incoming_data(struct telnet_session_s *t)
-{
+static void read_incoming_data(struct telnet_session_s *t) {
     size_t read_ret;
     char buf[BUF_LEN];
     size_t i;
@@ -257,77 +254,75 @@ static void read_incoming_data(struct telnet_session_s *t)
         t->in_eof = 1;
     } else {
         for (i=0; i<read_ret; i++) {
-	    process_incoming_char(t, (unsigned char)buf[i]);
-	}
+            process_incoming_char(t, (unsigned char)buf[i]);
+        }
     }
 }
 
 
 /* process a single character */
-static void process_incoming_char(struct telnet_session_s *t, int c)
-{
+static void process_incoming_char(struct telnet_session_s *t, int c) {
     switch (t->in_status) {
-        case GOT_IAC:
-	    switch (c) {
-                case WILL:
-		    t->in_status = GOT_WILL;
-		    break;
-                case WONT:
-		    t->in_status = GOT_WONT;
-		    break;
-                case DO:
-		    t->in_status = GOT_DO;
-		    break;
-                case DONT:
-		    t->in_status = GOT_DONT;
-		    break;
-	        case IAC:
-	            add_incoming_char(t, IAC);
-		    t->in_status = NORMAL;
-		    break;
-                default:
-		    t->in_status = NORMAL;
-	    }
-	    break;
-        case GOT_WILL:
-	    add_outgoing_char(t, IAC);
-	    add_outgoing_char(t, DONT);
-	    add_outgoing_char(t, c);
-	    t->in_status = NORMAL;
-	    break;
-        case GOT_WONT:
-	    t->in_status = NORMAL;
-	    break;
-        case GOT_DO:
-	    add_outgoing_char(t, IAC);
-	    add_outgoing_char(t, WONT);
-	    add_outgoing_char(t, c);
-	    t->in_status = NORMAL;
-	    break;
-        case GOT_DONT:
-	    t->in_status = NORMAL;
-	    break;
-        case GOT_CR:
-	    add_incoming_char(t, '\012');
-            if (c != '\012') {
-	        add_incoming_char(t, c);
-	    }
-	    t->in_status = NORMAL;
-	    break;
-	case NORMAL:
-            if (c == IAC) {
-	        t->in_status = GOT_IAC;
- 	    } else if (c == '\015') {
-	        t->in_status = GOT_CR;
-            } else {
-	        add_incoming_char(t, c);
-	    }
+    case GOT_IAC:
+        switch (c) {
+        case WILL:
+            t->in_status = GOT_WILL;
+            break;
+        case WONT:
+            t->in_status = GOT_WONT;
+            break;
+        case DO:
+            t->in_status = GOT_DO;
+            break;
+        case DONT:
+            t->in_status = GOT_DONT;
+            break;
+        case IAC:
+            add_incoming_char(t, IAC);
+            t->in_status = NORMAL;
+            break;
+        default:
+            t->in_status = NORMAL;
+        }
+        break;
+    case GOT_WILL:
+        add_outgoing_char(t, IAC);
+        add_outgoing_char(t, DONT);
+        add_outgoing_char(t, c);
+        t->in_status = NORMAL;
+        break;
+    case GOT_WONT:
+        t->in_status = NORMAL;
+        break;
+    case GOT_DO:
+        add_outgoing_char(t, IAC);
+        add_outgoing_char(t, WONT);
+        add_outgoing_char(t, c);
+        t->in_status = NORMAL;
+        break;
+    case GOT_DONT:
+        t->in_status = NORMAL;
+        break;
+    case GOT_CR:
+        add_incoming_char(t, '\012');
+        if (c != '\012') {
+            add_incoming_char(t, c);
+        }
+        t->in_status = NORMAL;
+        break;
+    case NORMAL:
+        if (c == IAC) {
+            t->in_status = GOT_IAC;
+        } else if (c == '\015') {
+            t->in_status = GOT_CR;
+        } else {
+            add_incoming_char(t, c);
+        }
     }
 }
 
 /* add a single character, wrapping buffer if necessary (should never occur) */
-static void add_incoming_char(struct telnet_session_s *t, int c)
-{
+static void add_incoming_char(struct telnet_session_s *t, int c) {
     daemon_assert(t->in_add >= 0);
     daemon_assert(t->in_add < BUF_LEN);
     t->in_buf[t->in_add] = c;
@@ -337,17 +332,16 @@ static void add_incoming_char(struct telnet_session_s *t, int c)
     }
     if (t->in_buflen == BUF_LEN) {
         t->in_take++;
-	if (t->in_take == BUF_LEN) {
-	    t->in_take = 0;
-	}
+        if (t->in_take == BUF_LEN) {
+            t->in_take = 0;
+        }
     } else {
         t->in_buflen++;
     }
 }
 
 /* remove a single character */
-static int use_incoming_char(struct telnet_session_s *t)
-{
+static int use_incoming_char(struct telnet_session_s *t) {
     int c;
 
     daemon_assert(t->in_take >= 0);
@@ -363,8 +357,7 @@ static int use_incoming_char(struct telnet_session_s *t)
 }
 
 /* add a single character, hopefully will never happen :) */
-static void add_outgoing_char(struct telnet_session_s *t, int c)
-{
+static void add_outgoing_char(struct telnet_session_s *t, int c) {
     daemon_assert(t->out_add >= 0);
     daemon_assert(t->out_add < BUF_LEN);
     t->out_buf[t->out_add] = c;
@@ -374,22 +367,21 @@ static void add_outgoing_char(struct telnet_session_s *t, int c)
     }
     if (t->out_buflen == BUF_LEN) {
         t->out_take++;
-	if (t->out_take == BUF_LEN) {
-	    t->out_take = 0;
-	}
+        if (t->out_take == BUF_LEN) {
+            t->out_take = 0;
+        }
     } else {
         t->out_buflen++;
     }
 }
 
 
-static void write_outgoing_data(struct telnet_session_s *t)
-{
+static void write_outgoing_data(struct telnet_session_s *t) {
     size_t write_ret;
 
     if (t->out_take < t->out_add) {
         /* handle a buffer that looks like this:       */
-	/*     |-- empty --|-- data --|-- empty --|    */
+        /*     |-- empty --|-- data --|-- empty --|    */
         daemon_assert(t->out_take >= 0);
         daemon_assert(t->out_take < BUF_LEN);
         daemon_assert(t->out_buflen > 0);
@@ -397,13 +389,13 @@ static void write_outgoing_data(struct telnet_session_s *t)
         write_ret = write(t->out_fd, t->out_buf + t->out_take, t->out_buflen);
     } else {
         /* handle a buffer that looks like this:       */
-	/*     |-- data --|-- empty --|-- data --|     */
+        /*     |-- data --|-- empty --|-- data --|     */
         daemon_assert(t->out_take >= 0);
         daemon_assert(t->out_take < BUF_LEN);
         daemon_assert((BUF_LEN - t->out_take) > 0);
         write_ret = write(t->out_fd, 
-	                  t->out_buf + t->out_take, 
-			  BUF_LEN - t->out_take);
+            t->out_buf + t->out_take,
+            BUF_LEN - t->out_take);
     }
 
     /* handle three possible write results */
@@ -413,16 +405,15 @@ static void write_outgoing_data(struct telnet_session_s *t)
         t->out_eof = 1;
     } else {
         t->out_buflen -= write_ret;
-	t->out_take += write_ret;
-	if (t->out_take >= BUF_LEN) {
-	    t->out_take -= BUF_LEN;
-	}
+        t->out_take += write_ret;
+        if (t->out_take >= BUF_LEN) {
+            t->out_take -= BUF_LEN;
+        }
     }
 }
 
 /* return the amount of a read */
-static int max_input_read(struct telnet_session_s *t)
-{
+static int max_input_read(struct telnet_session_s *t) {
     int max_in;
     int max_out;
 
@@ -449,8 +440,7 @@ static int max_input_read(struct telnet_session_s *t)
 }
 
 #ifndef NDEBUG
-static int invariant(const struct telnet_session_s *t)
-{
+static int invariant(const struct telnet_session_s *t) {
     if (t == NULL) {
         return 0;
     }
@@ -502,54 +492,3 @@ static int invariant(const struct telnet_session_s *t)
     return 1;
 }
 #endif /* NDEBUG */
-
-#ifdef STUB_TEST
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <signal.h>
-#include <ctype.h>
-
-int main()
-{
-    struct telnet_session_s t;
-    char buf[64];
-    int fd;
-    int val;
-    struct sockaddr_in addr;
-    int newfd;
-    struct sockaddr_in newaddr;
-    unsigned newaddrlen;
-    int i;
-
-    daemon_assert((fd = socket(AF_INET, SOCK_STREAM, 0)) != -1);
-    val = 1;
-    daemon_assert(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) 
-        == 0);
-    addr.sin_port = htons(23);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    daemon_assert(bind(fd, &addr, sizeof(addr)) == 0);
-    daemon_assert(listen(fd, SOMAXCONN) == 0);
-
-    signal(SIGPIPE, SIG_IGN);
-    while ((newfd = accept(fd, &newaddr, &newaddrlen)) >= 0) {
-        telnet_session_init(&t, newfd, newfd);
-        telnet_session_print(&t, "Password:");
-        telnet_session_readln(&t, buf, sizeof(buf));
-	for (i=0; buf[i] != '\0'; i++) {
-	    printf("[%02d]: 0x%02X ", i, buf[i] & 0xFF);
-	    if (isprint(buf[i])) {
-	        printf("'%c'\n", buf[i]);
-	    } else {
-	        printf("'\\x%02X'\n", buf[i] & 0xFF);
-	    }
-	}
-        telnet_session_println(&t, "Hello, world.");
-        telnet_session_println(&t, "Ipso, facto");
-        telnet_session_println(&t, "quid pro quo");
-        sleep(1);
-        close(newfd);
-    }
-    return 0;
-}
-#endif /* STUB_TEST */
-
