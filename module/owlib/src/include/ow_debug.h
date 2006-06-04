856df88cb9fa1a155431a50c46c5e7c9c5e78a24
Christian Magnusson <mag@mag.cx>
2006-06-04 20:49:43 +0000

    Add option to remove all debug-output... --disable-debug Should perhaps one level with output even if this option is used... but this is a start...

--- module/owlib/src/include/ow_debug.h
+++ module/owlib/src/include/ow_debug.h
@@ -13,6 +13,9 @@
 #ifndef OW_DEBUG_H
 #define OW_DEBUG_H
 
+#include <config.h>
+#include <owfs_config.h>
+
 /* error functions */
 void err_msg(int errnoflag, int level, const char *fmt, ...) ;
 
@@ -21,6 +24,7 @@ extern int error_level ;
 extern int now_background ;
 extern int log_available ;
 
+#if DEBUG
 #define LEVEL_DEFAULT(...)    if (error_level>0) err_msg(0,0,__VA_ARGS__) ;
 #define LEVEL_CONNECT(...)    if (error_level>1) err_msg(0,1,__VA_ARGS__) ;
 #define LEVEL_CALL(...)       if (error_level>2) err_msg(0,2,__VA_ARGS__) ;
@@ -34,6 +38,21 @@ extern int log_available ;
 #define ERROR_DATA(...)       if (error_level>5) err_msg(1,3,__VA_ARGS__) ;
 #define ERROR_DETAIL(...)     if (error_level>5) err_msg(1,4,__VA_ARGS__) ;
 #define ERROR_DEBUG(...)      if (error_level>5) err_msg(1,5,__VA_ARGS__) ;
+#else
+#define LEVEL_DEFAULT(...)    { } while (0);
+#define LEVEL_CONNECT(...)    { } while (0);
+#define LEVEL_CALL(...)       { } while (0);
+#define LEVEL_DATA(...)       { } while (0);
+#define LEVEL_DETAIL(...)     { } while (0);
+#define LEVEL_DEBUG(...)      { } while (0);
+
+#define ERROR_DEFAULT(...)    { } while (0);
+#define ERROR_CONNECT(...)    { } while (0);
+#define ERROR_CALL(...)       { } while (0);
+#define ERROR_DATA(...)       { } while (0);
+#define ERROR_DETAIL(...)     { } while (0);
+#define ERROR_DEBUG(...)      { } while (0);
+#endif
 
 /* Make sure strings are safe for printf */
 #define SAFESTRING(x) ((x) ? (x):"")
