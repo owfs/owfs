6dd83ba4bd8ebcaf784debb7fbd3301d4703c92f
Paul Alfille <paul.alfille@gmail.com>
2006-11-05 18:15:34 +0000

    Add dns_sd (Bonjour) headers to owshell

---
+++ module/owshell/src/include/ow_dl.h
@@ -0,0 +1,35 @@
+/*
+$Id$
+    OW -- One-Wire filesystem
+    version 0.4 7/2/2003
+
+    All the error and statistics counters are declared here,
+   including handling macros
+*/ 
+
+#ifndef OW_DL_H
+#define OW_DL_H
+
+#include <config.h>
+#include "owfs_config.h"
+
+#if OW_CYGWIN
+
+#include <windows.h>
+typedef HMODULE DLHANDLE;
+
+#elif defined(HAVE_DLOPEN)
+
+#include <dlfcn.h>
+typedef void * DLHANDLE;
+
+#endif
+
+extern DLHANDLE libdnssd;
+
+DLHANDLE DL_open(const char *pathname, int mode);
+void *DL_sym(DLHANDLE handle, const char *name);
+int DL_close(DLHANDLE handle);
+char *DL_error(void);
+
+#endif
