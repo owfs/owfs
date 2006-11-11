193d917391ed2b55ea49affd29571b423f2f4efd
Christian Magnusson <mag@mag.cx>
2006-11-11 06:57:41 +0000

    Sorry... duplicate files are needed in owshell for some make-versions...

---
+++ module/owshell/src/c/ow_dl.c
@@ -0,0 +1,55 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2003 Paul H Alfille
+    email: palfille@earthlink.net
+    Released under the GPL
+    See the header file: ow.h for full attribution
+    1wire/iButton system from Dallas Semiconductor
+*/
+
+#include <config.h>
+#include "owfs_config.h"
+#include "ow_dl.h"
+
+DLHANDLE DL_open(const char *pathname, int mode)
+{
+#if OW_CYGWIN
+  return LoadLibrary(pathname);
+#elif defined(HAVE_DLOPEN)
+  return dlopen(pathname, mode);
+#endif
+
+}
+
+void *DL_sym(DLHANDLE handle, const char *name)
+{
+#if OW_CYGWIN
+  return (void *) GetProcAddress(handle, name);
+#elif defined(HAVE_DLOPEN)
+  return dlsym(handle, name);
+#endif
+
+}
+
+
+int DL_close(DLHANDLE handle)
+{
+#if OW_CYGWIN
+  if (FreeLibrary(handle)) return 0;
+  return -1;
+#elif defined(HAVE_DLOPEN)
+  return dlclose(handle);
+#endif
+}
+
+char *DL_error(void)
+{
+#if OW_CYGWIN
+  return "Error in WIN32 DL";
+#elif defined(HAVE_DLOPEN)
+  return dlerror();
+#endif
+}
+
---
+++ module/owshell/src/c/ow_dnssd.c
@@ -0,0 +1,134 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2006 Paul H Alfille
+	email: palfille@earthlink.net
+	Released under the GPL
+	See the header file: ow.h for full attribution
+	1wire/iButton system from Dallas Semiconductor
+*/
+
+#include <config.h>
+#include "owfs_config.h"
+#include "ow.h"
+
+#include "ow_dl.h"
+#include "ow_dnssd.h"
+
+#if OW_CYGWIN
+
+#ifdef HAVE_DLFCN_H
+#include <dlfcn.h>
+#endif
+
+#endif
+
+DLHANDLE libdnssd = NULL;
+
+_DNSServiceRefSockFD DNSServiceRefSockFD;
+_DNSServiceProcessResult DNSServiceProcessResult;
+_DNSServiceRefDeallocate DNSServiceRefDeallocate;
+_DNSServiceResolve DNSServiceResolve;
+_DNSServiceBrowse DNSServiceBrowse;
+_DNSServiceRegister DNSServiceRegister;
+_DNSServiceReconfirmRecord DNSServiceReconfirmRecord;
+_DNSServiceCreateConnection DNSServiceCreateConnection;
+_DNSServiceEnumerateDomains DNSServiceEnumerateDomains;
+
+int OW_Load_dnssd_library(void) {
+#if OW_CYGWIN
+  int i;
+  char libdirs[3][80] = {
+    //{ "/opt/owfs/lib/libdns_sd.dll" },
+    { "libdns_sd.dll" },
+    { "" }
+  };
+
+  i = 0;
+  while(*libdirs[i]) {
+    /* Cygwin has dlopen and it seems to be ok to use it actually. */
+    if (!(libdnssd = DL_open(libdirs[i], 0))) {
+      /* Couldn't open that lib, but continue anyway */
+#if 0
+      char *derr;
+      derr = DL_error();
+      fprintf(stderr, "dlopen [%s] failed [%s]\n", libdirs[i], derr);
+#endif
+      i++;
+      continue;
+    } else {
+#if 0
+      fprintf(stderr, "dlopen [%s] success\n", libdirs[i]);
+#endif
+      break;
+    }
+  }
+#if 0
+  /* This file compiled with Microsoft Visual C doesn't work actually... */
+  if(!libdnssd) {
+    char file[255];
+    strcpy(file, "dnssd.dll");
+    if (!(libdnssd = DL_open(file, 0))) {
+      /* Couldn't open that lib, but continue anyway */
+#if 0
+      char *derr;
+      derr = DL_error();
+      fprintf(stderr, "DL_open [%s] failed [%s]\n", file, derr);
+#endif
+    }
+  }
+#endif
+
+#elif defined(HAVE_DLOPEN)
+  int i;
+  char libdirs[3][80] = {
+    { "/opt/owfs/lib/libdns_sd.so" },
+    { "libdns_sd.so" },
+    { "" }
+  };
+  
+  i = 0;
+  while(*libdirs[i]) {
+    if (!(libdnssd = DL_open(libdirs[i], RTLD_LAZY))) {
+      /* Couldn't open that lib, but continue anyway */
+#if 0
+      char *derr;
+      derr = DL_error();
+      fprintf(stderr, "DL_open [%s] failed [%s]\n", libdirs[i], derr);
+#endif
+      i++;
+      continue;
+    } else {
+#if 0
+      fprintf(stderr, "DL_open [%s] success\n", libdirs[i]);
+#endif
+      break;
+    }
+  }
+#endif
+
+  if(libdnssd == NULL) {
+    //fprintf(stderr, "Zeroconf/Bonjour is disabled since dnssd library is not found\n");
+    return -1;
+  }
+
+  DNSServiceRefSockFD = (_DNSServiceRefSockFD)DL_sym(libdnssd, "DNSServiceRefSockFD");
+  DNSServiceProcessResult = (_DNSServiceProcessResult)DL_sym(libdnssd, "DNSServiceProcessResult");
+  DNSServiceRefDeallocate = (_DNSServiceRefDeallocate)DL_sym(libdnssd, "DNSServiceRefDeallocate");
+  DNSServiceResolve = (_DNSServiceResolve)DL_sym(libdnssd, "DNSServiceResolve");
+  DNSServiceBrowse = (_DNSServiceBrowse)DL_sym(libdnssd, "DNSServiceBrowse");
+  DNSServiceRegister  = (_DNSServiceRegister)DL_sym(libdnssd, "DNSServiceRegister");
+  DNSServiceReconfirmRecord = (_DNSServiceReconfirmRecord)DL_sym(libdnssd, "DNSServiceReconfirmRecord");
+  DNSServiceCreateConnection = (_DNSServiceCreateConnection)DL_sym(libdnssd, "DNSServiceCreateConnection");
+  DNSServiceEnumerateDomains = (_DNSServiceEnumerateDomains)DL_sym(libdnssd, "DNSServiceEnumerateDomains");
+
+  return 0;
+}
+
+int OW_Free_dnssd_library(void) {
+  int rc = -1;
+  if(libdnssd)  rc = DL_close(libdnssd);
+  libdnssd = NULL;
+  return rc;
+}
