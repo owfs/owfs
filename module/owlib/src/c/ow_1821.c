9b0282bb0ca36d15d3e16556a9becfe3c53394c0
Paul Alfille <paul.alfille@gmail.com>
2007-03-27 03:29:34 +0000

    Start substituting constants for field lengths

--- module/owlib/src/c/ow_1821.c
+++ module/owlib/src/c/ow_1821.c
@@ -55,10 +55,10 @@ WRITE_FUNCTION(FS_w_templimit);
 /* -------- Structures ---------- */
 struct filetype DS1821[] = {
 	F_type,
-  {"temperature", 12, NULL, ft_temperature, fc_volatile, {o: FS_temperature}, {v: NULL}, {v:NULL},},
-  {"polarity", 1, NULL, ft_yesno, fc_stable, {o: FS_r_polarity}, {o: FS_w_polarity}, {v:NULL},},
-  {"templow", 12, NULL, ft_temperature, fc_stable, {o: FS_r_templimit}, {o: FS_w_templimit}, {i:1},},
-  {"temphigh", 12, NULL, ft_temperature, fc_stable, {o: FS_r_templimit}, {o: FS_w_templimit}, {i:0},},
+  {"temperature", PROPERTY_LENGTH_FLOAT, NULL, ft_temperature, fc_volatile, {o: FS_temperature}, {v: NULL}, {v:NULL},},
+  {"polarity", PROPERTY_LENGTH_YESNO, NULL, ft_yesno, fc_stable, {o: FS_r_polarity}, {o: FS_w_polarity}, {v:NULL},},
+  {"templow", PROPERTY_LENGTH_FLOAT, NULL, ft_temperature, fc_stable, {o: FS_r_templimit}, {o: FS_w_templimit}, {i:1},},
+  {"temphigh", PROPERTY_LENGTH_FLOAT, NULL, ft_temperature, fc_stable, {o: FS_r_templimit}, {o: FS_w_templimit}, {i:0},},
 }
 
 ;
