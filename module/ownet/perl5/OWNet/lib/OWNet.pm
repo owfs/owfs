ac26dcf11aa0eba9580219e6fdcddd9e4e136a9a
Christian Magnusson <mag@mag.cx>
2007-03-30 11:26:50 +0000

    Change timeout to 3000ms

--- module/ownet/perl5/OWNet/lib/OWNet.pm
+++ module/ownet/perl5/OWNet/lib/OWNet.pm
@@ -329,7 +329,7 @@ sub _FromServerLow($$) {
     my $fullread = '' ;
     #print "LOOOP for length $length \n" ;
     do {
-        select($selectreadbits,undef,undef,1) ;
+        select($selectreadbits,undef,undef,3000) ;  // 3000ms timeout
         return if vec($selectreadbits,$fileno,1) == 0 ;
     #	return if $sel->can_read(1) == 0 ;
         my $partialread ;
