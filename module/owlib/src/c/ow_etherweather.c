d2662fb98109f7bb2be1d9a9739d74d991ce24f8
Jacob Potter <jacobdp@gmail.com>
2007-05-07 17:52:42 +0000

    Add CVS ID and license lines to ow_etherweather.c

--- module/owlib/src/c/ow_etherweather.c
+++ module/owlib/src/c/ow_etherweather.c
@@ -1,3 +1,14 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2003 Paul H Alfille
+    email: palfille@earthlink.net
+    Released under the GPL
+    See the header file: ow.h for full attribution
+    1wire/iButton system from Dallas Semiconductor
+*/
+
 #include <config.h>
 #include "owfs_config.h"
 #include "ow.h"
