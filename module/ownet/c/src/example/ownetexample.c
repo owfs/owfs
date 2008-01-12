a5984dcb3c6cc9a862d7d8c8c093ab7a2d75ff04
Paul Alfille <paul.alfille@gmail.com>
2008-01-12 21:45:22 +0000

    Update ownetlib examples

--- module/ownet/c/src/example/ownetexample.c
+++ module/ownet/c/src/example/ownetexample.c
@@ -4,46 +4,74 @@
 
 #include <ownetapi.h>
 
-const char DEFAULT_ARGV[] = "4304";
+//------------- Global vaiables ----------
+char * owserver_address = "4304";
+char * one_wire_path = "/";
 
-const char DEFAULT_PATH[] = "/";
+//------------- Usage information --------
+void usage( int argc, char ** argv )
+{
+    printf("%s shows examples of directory and read functions\n",basename(argv[0]));
+    printf("\n");
+    printf("Usage of %s:\n",basename(argv[0]));
+    printf("\t%s -s owserver_address one_wire_path\n",argv[0]);
+    printf("\t\towserver_address -- tcp/ip address:port of owserver\n");
+    printf("\t\t\te.g. 192.168.0.77:3000 or just port number\n");
+    printf("\t\t\tdefault localhost:4304\n");
+    printf("\t\tone_wire_path    -- OWFS-style 1-wire address.\n");
+    printf("\t\t\te.g.: / for root 1-wire bus listing (default)\n");
+    printf("\t\t\tor /10.67C6697351FF for a device directory\n");
+    printf("\n");
+    printf("see http://www.owfs.org for information on owserver.\n");
+    exit(1) ;
+}
 
+//------------- Command line parsing -----
+void parse_command_line( int argc, char ** argv )
+{
+    int argc_index ;
+    int next_is_owserver = 0 ;
+    if ( argc == 1 ) {
+        usage(argc,argv);
+    }
+    for (argc_index = 1 ; argc_index < argc ; ++argc_index ) {
+        if ( strcmp(argv[argc_index],"-h")==0) {
+            usage(argc,argv) ;
+        } else if ( strcmp(argv[argc_index],"--help")==0) {
+            usage(argc,argv) ;
+        } else if ( strcmp(argv[argc_index],"-s")==0) {
+             next_is_owserver = 1 ;
+        } else if ( strcmp(argv[argc_index],"--server")==0) {
+             next_is_owserver = 1 ;
+        } else {
+            if ( next_is_owserver ) {
+                owserver_address = argv[argc_index] ;
+                next_is_owserver = 0 ;
+            } else {
+                one_wire_path = argv[argc_index] ;
+            }
+        }
+    }
+}
+
+//------------- Example-specific ---------
 int main(int argc, char **argv)
 {
   ssize_t rc;
   ssize_t ret = 0;
   size_t len;
   char *buffer = NULL;
-  const char *path = DEFAULT_PATH;
   OWNET_HANDLE owh;
 
-  printf("Usage example:\n");
-  printf("%s\n", argv[0]);
-  printf("%s /10.67C6697351FF\n", argv[0]);
-  printf("%s / 6666\n", argv[0]);
-  printf("Default parameters are:  %s %s %s\n", argv[0], DEFAULT_PATH, DEFAULT_ARGV);
+    parse_command_line(argc,argv) ;
 
-  /* steal first argument and treat it as a path (if not beginning with '-') */
-  if((argc > 1) && (argv[1][0] != '-')) {
-    path = argv[1];
-    argv = &argv[1];
-    argc--;
-  }
-
-  if(argc < 2) {
-    printf("Starting with extra parameter \"%s\" as default\n", DEFAULT_ARGV);
-    if((owh = OWNET_init(DEFAULT_ARGV)) < 0) {
-      perror("OWNET_init failed.\n");
-      goto cleanup;
+    if((owh = OWNET_init(owserver_address)) < 0) {
+        printf("OWNET_init(%s) failed.\n", owserver_address);
+        exit(1) ;
     }
-  } else {
-    if((owh = OWNET_init(argv[1])) < 0) {
-      printf("OWNET_init(%s) failed.\n", argv[1]);
-      goto cleanup;
-    }
-  }
 
-  len = OWNET_dirlist(owh, path, &buffer);
+
+  len = OWNET_dirlist(owh, one_wire_path, &buffer);
   if((len<0) || !buffer) {
     printf("OWNET_dirlist: buffer is empty (buffer=%p len=%d)\n", buffer, len);
     goto cleanup;
@@ -84,4 +112,3 @@ int main(int argc, char **argv)
 
   exit(ret);
 }
-
