b001c919aee95fd3883e04ae27e27329b78dae1a
Doug Collinge <doug.collinge@gmail.com>
2008-01-22 06:17:55 +0000

    Completed full datasheet support. New properties added: templowflag, temphighflag, thermostatmode, 1shot. Re-encoded the status register bits with defines. Made "continuous mode" actually work. Temperature precision is 1 degree C in continuous mode but 1/16 C in one-shot mode.

--- module/owlib/src/c/ow_1821.c
+++ module/owlib/src/c/ow_1821.c
@@ -41,7 +41,7 @@ $Id$
 
 #include <config.h>
 #include "owfs_config.h"
-#include "ow_1820.h"
+#include "ow_1821.h"
 
 /* ------- Prototypes ----------- */
 /* DS18S20&2 Temperature */
@@ -51,6 +51,14 @@ READ_FUNCTION(FS_r_polarity);
 WRITE_FUNCTION(FS_w_polarity);
 READ_FUNCTION(FS_r_templimit);
 WRITE_FUNCTION(FS_w_templimit);
+READ_FUNCTION(FS_r_thermomode);
+WRITE_FUNCTION(FS_w_thermomode);
+READ_FUNCTION(FS_r_oneshot);
+WRITE_FUNCTION(FS_w_oneshot);
+READ_FUNCTION(FS_r_tlf);
+WRITE_FUNCTION(FS_w_tlf);
+READ_FUNCTION(FS_r_thf);
+WRITE_FUNCTION(FS_w_thf);
 
 /* -------- Structures ---------- */
 struct filetype DS1821[] = {
@@ -59,6 +67,10 @@ struct filetype DS1821[] = {
   {"polarity",PROPERTY_LENGTH_YESNO, NULL, ft_yesno, fc_stable,   FS_r_polarity, FS_w_polarity, {v:NULL},} ,
   {"templow",PROPERTY_LENGTH_TEMP, NULL, ft_temperature, fc_stable,   FS_r_templimit, FS_w_templimit, {i:1},} ,
   {"temphigh",PROPERTY_LENGTH_TEMP, NULL, ft_temperature, fc_stable,   FS_r_templimit, FS_w_templimit, {i:0},} ,
+  {"templowflag",PROPERTY_LENGTH_YESNO, NULL, ft_yesno, fc_stable,   FS_r_tlf, FS_w_tlf, {v:NULL},},
+  {"temphighflag",PROPERTY_LENGTH_YESNO, NULL, ft_yesno, fc_stable,   FS_r_thf, FS_w_thf, {v:NULL},},
+  {"thermostatmode",PROPERTY_LENGTH_YESNO, NULL, ft_yesno, fc_stable,   FS_r_thermomode, FS_w_thermomode, {v:NULL},},
+  {"1shot",PROPERTY_LENGTH_YESNO, NULL, ft_yesno, fc_stable,   FS_r_oneshot, FS_w_oneshot, {v:NULL},} 
 }
 
 ;
@@ -76,12 +88,27 @@ DeviceEntry(thermostat, DS1821);
 #define _1W_READ_COUNTER 0xA0
 #define _1W_LOAD_COUNTER 0x41
 
+	/* Definition of status register bits. */
+	/* Please see the DS1821 datasheet for further information. */
+	#define DS1821_STATUS_1SHOT 0	// 0: convert temp continuously; 1: one conversion only.
+	#define DS1821_STATUS_POL 1	// 0: in thermostat mode DQ output is active low; 1: active high.
+	#define DS1821_STATUS_TR 2	// 0: go 1-wire mode on next power-up; 1: go into thermostat mode.
+	#define DS1821_STATUS_TLF 3	// 0: temp has not been below the TL temp; 1: temp has.
+	#define DS1821_STATUS_THF 4	// 0: temp has not been above the TH temp; 1: temp has.
+	#define DS1821_STATUS_NVB 5	// 0: EEPROM write not in progress; 1: EEPROM write in progress.
+					// Bit 6 always returns 1.
+	#define DS1821_STATUS_DONE 7	// 0: temp conversion in progress; 1: conversion finished.
+	/* End of status register bits. */
+	
+
 /* ------- Functions ------------ */
 
 /* DS1821*/
 static int OW_temperature(_FLOAT * temp, const struct parsedname *pn);
 static int OW_current_temperature(_FLOAT * temp,
 								  const struct parsedname *pn);
+static int OW_current_temperature_lowres(_FLOAT * temp,
+									  const struct parsedname *pn);
 static int OW_r_status(BYTE * data, const struct parsedname *pn);
 static int OW_w_status(BYTE * data, const struct parsedname *pn);
 static int OW_r_templimit(_FLOAT * T, const int Tindex,
@@ -100,13 +127,85 @@ static int FS_temperature(struct one_wire_query * owq)
 	return 0;
 }
 
-static int FS_r_polarity(struct one_wire_query * owq)
+static int FS_r_thf(struct one_wire_query * owq)
+{
+    BYTE data;
+
+    if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+    OWQ_Y(owq) = (data >> DS1821_STATUS_THF) & 0x01;
+//    OWQ_Y(owq) = (data & 0x10) >> 4;
+	return 0;
+}
+
+static int FS_w_thf(struct one_wire_query * owq)
 {
 	BYTE data;
 
     if (OW_r_status(&data, PN(owq)))
 		return -EINVAL;
-    OWQ_Y(owq) = (data & 0x02) >> 1;
+    UT_setbit(&data, DS1821_STATUS_THF, OWQ_Y(owq));
+    if (OW_w_status(&data, PN(owq)))
+		return -EINVAL;
+	return 0;
+}
+
+
+static int FS_r_tlf(struct one_wire_query * owq)
+{
+    BYTE data;
+
+    if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+    OWQ_Y(owq) = (data >> DS1821_STATUS_TLF) & 0x01;
+//OWQ_Y(owq) = (data & 0x08) >> 3;
+	return 0;
+}
+
+static int FS_w_tlf(struct one_wire_query * owq)
+{
+    BYTE data;
+
+    if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+    UT_setbit(&data, DS1821_STATUS_TLF, OWQ_Y(owq));
+    if (OW_w_status(&data, PN(owq)))
+		return -EINVAL;
+	return 0;
+}
+
+
+static int FS_r_thermomode(struct one_wire_query * owq)
+{
+    BYTE data;
+
+    if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+    OWQ_Y(owq) = (data >> DS1821_STATUS_TR) & 0x01;
+//    OWQ_Y(owq) = (data & 0x04) >> 2;
+	return 0;
+}
+
+static int FS_w_thermomode(struct one_wire_query * owq)
+{
+	BYTE data;
+
+    if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+    UT_setbit(&data, DS1821_STATUS_TR, OWQ_Y(owq));
+    if (OW_w_status(&data, PN(owq)))
+		return -EINVAL;
+	return 0;
+}
+
+
+static int FS_r_polarity(struct one_wire_query * owq)
+{
+    BYTE data;
+
+    if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+    OWQ_Y(owq) = (data >> DS1821_STATUS_POL) & 0x01;
 	return 0;
 }
 
@@ -116,9 +215,91 @@ static int FS_w_polarity(struct one_wire_query * owq)
 
     if (OW_r_status(&data, PN(owq)))
 		return -EINVAL;
-    UT_setbit(&data, 1, OWQ_Y(owq));
+    UT_setbit(&data, DS1821_STATUS_POL, OWQ_Y(owq));
     if (OW_w_status(&data, PN(owq)))
 		return -EINVAL;
+	return 0;
+}
+
+
+static int FS_r_oneshot(struct one_wire_query * owq)
+{
+	BYTE data;
+
+	if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+	OWQ_Y(owq) = (data >> DS1821_STATUS_1SHOT) & 0x01;
+
+	return 0;
+}
+
+/* Set or reset the "one shot" bit of the status register.
+ *
+ * One of several idiosyncrasies of this chip is it's "Continuous Conversion/1Shot" mode.
+ * If the 1Shot bit is zero at the time a temperature conversion is finished then the chip starts another
+ * conversion immediately without needing a Start Conversion command. It continues to do this until
+ * it hears a Stop Conversion command. This means that the chip doesn't do anything when the 1Shot
+ * bit is cleared - it waits until the next Start Conversion command before starting continuous conversions.
+ * I can't see how this waiting behaviour is useful so I have issued Start Conversion and Stop Conversion
+ * commands at the appropriate times on the assumption that the reason you are setting the continuous conversion
+ * mode is because you want continuous conversions.
+ */
+static int FS_w_oneshot(struct one_wire_query * owq)
+{
+	BYTE pstop[] = { _1W_STOP_CONVERT_T, };
+	struct transaction_log tstop[] = {
+		TRXN_START,
+		TRXN_WRITE1(pstop),
+		TRXN_END,
+	};
+
+	BYTE pstart[] = { _1W_START_CONVERT_T, };
+	struct transaction_log tstart[] = {
+		TRXN_START,
+		TRXN_WRITE1(pstart),
+		TRXN_END,
+	};
+
+	BYTE data;
+
+	if (OW_r_status(&data, PN(owq)))
+		return -EINVAL;
+
+	int oneshotmode = (data >> DS1821_STATUS_1SHOT) & 0x01 ;
+
+	UT_setbit(&data, DS1821_STATUS_1SHOT, OWQ_Y(owq));
+	if (OW_w_status(&data, PN(owq)))
+		return -EINVAL;
+
+	if ( !oneshotmode && OWQ_Y(owq) ) { 
+		/* 1Shot mode was off and we are setting it on; i.e. we are ending continuous mode.
+		 * Continuous conversions may be in progress.
+		 * Since we are turning this mode off, it's probably a reasonable assumption
+		 * that we don't want continuous conversions anymore, 
+		 * so issue a STOP CONVERSION to halt the conversions.
+		 */
+		int ret = BUS_transaction(tstop, PN(owq));
+		if( ret ) return ret;
+	}
+	else if ( oneshotmode && !OWQ_Y(owq) ) {
+		/* 1Shot mode was on and we are turning it off; i.e. we are starting continuous mode.
+		 * Continuous conversions are probably not in progress.
+		 * Presumably, we are doing this because we want continuous conversions so
+		 * issue a START CONVERSION command to kick things off.
+		 */
+		int ret = BUS_transaction(tstart, PN(owq));
+
+		/* We have just kicked off a conversion. If we try to read the temperature
+ 		 * right now we will get some previous temperature, which we are not probably
+ 		 * expecting.  So wait for the first conversion only, just in case. We will now
+ 		 * be in continuous conversion mode so after the first one we no longer have to 
+ 		 * worry about it.
+ 		 */
+		UT_delay(1000);
+		if( ret ) return ret;
+	}
+	// else ;	/* We are setting the bit to what it already was so do nothing special. */
+	
 	return 0;
 }
 
@@ -162,43 +343,45 @@ static int OW_w_status(BYTE * data, const struct parsedname *pn)
 	return BUS_transaction(t, pn);
 }
 
+/* Do a transaction to get the temperature, increasing the resolution if possible.
+ *
+ * When we are in continuous conversion mode we don't have to wait for a conversion to finish
+ * because, apart from the first one, one has always already been completed and the temperature stored in the
+ * temperature register.  
+ *
+ * However, a tricky wrinkle is that you cannot use the "high resolution" temperature calculation in continuous
+ * conversion mode because the counter is constantly in use and reading it disrupts the temperature measurement
+ * in progress. The result is incorrect temperature readings in the temperature register even if we don't attempt
+ * to use the counter value to increase resolution.
+ */
 static int OW_temperature(_FLOAT * temp, const struct parsedname *pn)
 {
     BYTE c[] = { _1W_START_CONVERT_T, };
 	BYTE status;
-	int continuous;
-	int need_to_trigger = 0;
+
 	struct transaction_log t[] = {
 		TRXN_START,
-        TRXN_WRITE1(c),
+	TRXN_WRITE1(c),
 		TRXN_END,
 	};
 
 	if (OW_r_status(&status, pn))
 		return 1;
 
-	if (status & 0x01) {		/* 1-shot, convert and wait 1 second */
-		need_to_trigger = 1;
-	} else {					/* continuous conversion mode */
-		if (Cache_Get_Internal_Strict
-			(&continuous, sizeof(continuous), InternalProp(CON), pn)
-			|| continuous == 0) {
-			need_to_trigger = 1;
-		}
-	}
-
-	if (need_to_trigger) {
+	if ((status >> DS1821_STATUS_1SHOT) & 0x01) {		/* 1-shot, convert and wait 1 second */
 		if (BUS_transaction(t, pn))
 			return 1;
 		UT_delay(1000);
-		if ((status & 0x01) == 0) {	/* continuous mode, just triggered */
-			continuous = 1;
-			Cache_Add_Internal(&continuous, sizeof(int), InternalProp(CON), pn);	/* Flag as continuous */
-		}
+		return OW_current_temperature(temp, pn);
+	} else {
+		/* in continuous conversion mode. No need to delay but can't get full resolution. */
+		return OW_current_temperature_lowres(temp, pn);
 	}
-	return OW_current_temperature(temp, pn);
 }
 
+/* Do a transaction to get the temperature, the remaining count, and the count-per-c registers.
+ * Perform the calculation to increase the temperature resolution.
+ */
 static int OW_current_temperature(_FLOAT * temp,
 								  const struct parsedname *pn)
 {
@@ -225,13 +408,38 @@ static int OW_current_temperature(_FLOAT * temp,
 
 	if (BUS_transaction(t, pn))
 		return 1;
+
 	if (count_per_c) {
+		/* Perform calculation to get high temperature resolution - see datasheet. */
 		temp[0] =
 			(_FLOAT) ((int8_t) temp_read) + .5 -
 			((_FLOAT) count_remain) / ((_FLOAT) count_per_c);
 	} else {					/* Bad count_per_c -- use lower resolution */
 		temp[0] = (_FLOAT) ((int8_t) temp_read);
 	}
+
+	return 0;
+}
+
+/* Read temperature register but do not attempt to increase the resolution,
+ * because, for instance, the chip is in continuous conversion mode.
+ */
+static int OW_current_temperature_lowres(_FLOAT * temp,
+								  const struct parsedname *pn)
+{
+    BYTE read_temp[] = { _1W_READ_TEMPERATURE, };
+	BYTE temp_read;
+	struct transaction_log t[] = {
+		TRXN_START,
+        TRXN_WRITE1(read_temp),
+        TRXN_READ1(&temp_read),
+		TRXN_END,
+	};
+
+	if (BUS_transaction(t, pn))
+		return 1;
+	temp[0] = (_FLOAT) ((int8_t) temp_read);
+
 	return 0;
 }
 
