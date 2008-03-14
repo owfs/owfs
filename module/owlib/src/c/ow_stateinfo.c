440a668fe629a4c541635aefd4ca7154474a7abb
Paul Alfille <paul.alfille@gmail.com>
2008-03-14 23:51:51 +0000

    Add ow_stateinfo.c to repository

---
+++ module/owlib/src/c/ow_stateinfo.c
@@ -0,0 +1,23 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2003 Paul H Alfille
+    email: palfille@earthlink.net
+    Released under the GPL
+    See the header file: ow.h for full attribution
+    1wire/iButton system from Dallas Semiconductor
+*/
+
+#include <config.h>
+#include "owfs_config.h"
+#include "ow.h"
+
+struct stateinfo StateInfo = {
+    lib_setup_done = 0 ,
+    lib_start_done = 0 ,
+    lib_finish_done = 0 ,
+    lock_setup_done = 0 ,
+    start_time = 0 ,
+    dir_time =0 ,
+};
