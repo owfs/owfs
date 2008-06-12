d667643c1fc826f31692f56e40393702b8e00a45
Christian Magnusson <mag@mag.cx>
2008-06-12 10:40:53 +0000

    Update perl-examples

--- module/swig/perl5/example/tree.pl
+++ module/swig/perl5/example/tree.pl
@@ -14,9 +14,10 @@ die (
     ."\t$0 1wire-adapter\n"
     ."  1wire-adapter (required):\n"
     ."\t'u' for USB -or-\n"
+    ."\t--fake=10,28 for a fake-adapter with DS18S20 and DS18B20-sensor -or-\n"
     ."\t/dev/ttyS0 (or whatever) serial port -or-\n"
-    ."\t1234 for remote-server at port 1234\n"
-    ."\t192.168.1.1:1234 for remote-server at 192.168.1.1:1234\n"
+    ."\t4304 for remote-server at port 4304\n"
+    ."\t192.168.1.1:4304 for remote-server at 192.168.1.1:4304\n"
     ) if ( $#ARGV != 0 ) ;
 
 OW::init($ARGV[0]) or die "Cannot open 1wire port $ARGV[0]" ;
