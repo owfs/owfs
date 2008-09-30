8918a988b27ef7a98d58defce83b8ae5bd68896c
Paul Alfille <paul.alfille@gmail.com>
2008-09-30 23:41:35 +0000

    Refactoring and fixed a memory leak in FS_OWQ_from_pn which was used by owhttpd.

--- module/owlib/src/c/ow_parseobject.c
+++ module/owlib/src/c/ow_parseobject.c
@@ -15,23 +15,36 @@ $Id$
 #include "ow_counters.h"
 #include "ow_connection.h"
 
+static int FS_OWQ_create_postparse(char *buffer, size_t size, off_t offset, const struct parsedname * pn, struct one_wire_query *owq) ;
+
 /* Create the Parsename structure and load the relevant fields */
 int FS_OWQ_create(const char *path, char *buffer, size_t size, off_t offset, struct one_wire_query *owq)
 {
 	struct parsedname *pn = PN(owq);
+	int return_code ;
 
 	LEVEL_DEBUG("FS_OWQ_create of %s\n", path);
-	if (FS_ParsedName(path, pn)) {
-		return -ENOENT;
+	
+	return_code = FS_ParsedName(path, pn) ;
+	if ( return_code == 0 ) {
+		return_code = FS_OWQ_create_postparse( buffer, size, offset, pn, owq ) ;
+		if ( return_code == 0 ) {
+			return 0 ;
+		}
+		FS_ParsedName_destroy(pn);
 	}
+	return return_code ;
+}
 
+static int FS_OWQ_create_postparse(char *buffer, size_t size, off_t offset, const struct parsedname * pn, struct one_wire_query *owq)
+{
 	OWQ_buffer(owq) = buffer;
 	OWQ_size(owq) = size;
 	OWQ_offset(owq) = offset;
+	memcpy(PN(owq), pn, sizeof(struct parsedname));
 	if (pn->extension == EXTENSION_ALL && pn->type != ePN_structure) {
 		OWQ_array(owq) = calloc((size_t) pn->selected_filetype->ag->elements, sizeof(union value_object));
 		if (OWQ_array(owq) == NULL) {
-			FS_ParsedName_destroy(pn);
 			return -ENOMEM;
 		}
 	} else {
@@ -42,25 +55,14 @@ int FS_OWQ_create(const char *path, char *buffer, size_t size, off_t offset, str
 
 struct one_wire_query *FS_OWQ_from_pn(const struct parsedname *pn)
 {
-	struct one_wire_query *owq;
 	size_t size = FullFileLength(pn);
 	char *buffer = malloc(size);
 
 	if (buffer != NULL) {
-		owq = malloc(sizeof(struct one_wire_query));
+		struct one_wire_query *owq = malloc(sizeof(struct one_wire_query));
 		if (owq != NULL) {
-			memcpy(PN(owq), pn, sizeof(struct parsedname));
-			OWQ_buffer(owq) = buffer;
-			OWQ_size(owq) = size;
-			OWQ_offset(owq) = 0;
-			if (pn->extension == EXTENSION_ALL && pn->type != ePN_structure) {
-				OWQ_array(owq) = calloc((size_t) pn->selected_filetype->ag->elements, sizeof(union value_object));
-				if (OWQ_array(owq) != NULL) {
-					return owq;
-				}
-			} else {
-				OWQ_I(owq) = 0;
-				return owq;
+			if ( FS_OWQ_create_postparse( buffer, size, 0, pn, owq ) == 0 ) {
+				return owq ;
 			}
 			free(owq);
 		}
@@ -74,11 +76,18 @@ void FS_OWQ_from_pn_destroy(struct one_wire_query *owq)
 	struct parsedname *pn = PN(owq);
 
 	LEVEL_DEBUG("FS_OWQ_from_pn_destroy of %s\n", pn->path);
+	
 	if (pn->extension == EXTENSION_ALL && pn->type != ePN_structure) {
 		if (OWQ_array(owq)) {
 			free(OWQ_array(owq));
 			OWQ_array(owq) = NULL;
 		}
+	}
+	if ( OWQ_buffer(owq) != NULL ) {
+		free( OWQ_buffer(owq) ) ;
+	}
+	if ( owq ) {
+		free(owq) ;
 	}
 }
 
@@ -86,24 +95,19 @@ void FS_OWQ_from_pn_destroy(struct one_wire_query *owq)
 int FS_OWQ_create_plus(const char *path, const char *file, char *buffer, size_t size, off_t offset, struct one_wire_query *owq)
 {
 	struct parsedname *pn = PN(owq);
+	int return_code ;
 
 	LEVEL_DEBUG("FS_OWQ_create_plus of %s + %s\n", path, file);
-	if (FS_ParsedNamePlus(path, file, pn)) {
-		return -ENOENT;
-	}
-	OWQ_buffer(owq) = buffer;
-	OWQ_size(owq) = size;
-	OWQ_offset(owq) = offset;
-	if (pn->extension == EXTENSION_ALL && pn->type != ePN_structure) {
-		OWQ_array(owq) = calloc((size_t) pn->selected_filetype->ag->elements, sizeof(union value_object));
-		if (OWQ_array(owq) == NULL) {
-			FS_ParsedName_destroy(pn);
-			return -ENOMEM;
+
+	return_code = FS_ParsedNamePlus(path, file, pn) ;
+	if ( return_code == 0 ) {
+		return_code = FS_OWQ_create_postparse( buffer, size, offset, pn, owq ) ;
+		if ( return_code == 0 ) {
+			return 0 ;
 		}
-	} else {
-		OWQ_I(owq) = 0;
+		FS_ParsedName_destroy(pn);
 	}
-	return 0;
+	return return_code ;
 }
 
 void FS_OWQ_destroy(struct one_wire_query *owq)
