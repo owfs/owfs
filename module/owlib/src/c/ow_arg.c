6607f81fab90479d16c7be95cbf0aa7b59e1d68d
Paul Alfille <paul.alfille@gmail.com>
2009-08-15 11:13:07 +0000

    Alphbetize the  ARG_*  entries.

--- module/owlib/src/c/ow_arg.c
+++ module/owlib/src/c/ow_arg.c
@@ -16,15 +16,62 @@ $Id$
 #include "ow.h"
 #include "ow_connection.h"
 
-int ARG_Net(const char *arg)
+int ARG_Device(const char *arg)
+{
+	struct stat sbuf;
+	if (stat(arg, &sbuf)) {
+		LEVEL_DEFAULT("Cannot access device %s\n", arg);
+		return 1;
+	}
+	if (!S_ISCHR(sbuf.st_mode)) {
+		LEVEL_DEFAULT("Not a \"character\" device %s (st_mode=%x)\n", arg, sbuf.st_mode);
+		return 1;
+	}
+	if (major(sbuf.st_rdev) == 99) {
+		return ARG_Parallel(arg);
+	}
+	if (major(sbuf.st_rdev) == 89) {
+		return ARG_I2C(arg);
+	}
+	return ARG_Serial(arg);
+}
+
+int ARG_EtherWeather(const char *arg)
+{
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
+	}
+	in->name = arg ? owstrdup(arg) : NULL;
+	in->busmode = bus_etherweather;
+	return 0;
+}
+
+int ARG_Fake(const char *arg)
 {
 	struct connection_in *in = NewIn(NULL);
 	if (in == NULL) {
 		return 1;
 	}
 	in->name = owstrdup(arg);
-	in->busmode = bus_server;
+	in->busmode = bus_fake;
 	return 0;
+}
+
+int ARG_Generic(const char *arg)
+{
+	if (arg && arg[0]) {
+		switch (arg[0]) {
+			case '/':
+				return ARG_Device(arg);
+			case 'u':
+			case 'U':
+				return ARG_USB(&arg[1]);
+			default:
+				return ARG_Net(arg);
+		}
+	}
+	return 1;
 }
 
 int ARG_HA5( const char *arg)
@@ -60,36 +107,45 @@ int ARG_HA7(const char *arg)
 #endif							/* OW_HA7 */
 }
 
-int ARG_EtherWeather(const char *arg)
-{
-	struct connection_in *in = NewIn(NULL);
-	if (in == NULL) {
-		return 1;
-	}
-	in->name = arg ? owstrdup(arg) : NULL;
-	in->busmode = bus_etherweather;
-	return 0;
-}
-
-int ARG_Xport(const char *arg)
-{
-	struct connection_in *in = NewIn(NULL);
-	if (in == NULL) {
-		return 1;
-	}
-	in->name = arg ? owstrdup(arg) : NULL;
-	in->busmode = bus_xport;
-	return 0;
-}
-
-int ARG_Fake(const char *arg)
+int ARG_HA7E(const char *arg)
 {
 	struct connection_in *in = NewIn(NULL);
 	if (in == NULL) {
 		return 1;
 	}
 	in->name = owstrdup(arg);
-	in->busmode = bus_fake;
+	in->busmode = bus_ha7e ;
+	return 0;
+}
+
+int ARG_I2C(const char *arg)
+{
+	#if OW_I2C
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
+	}
+	if ( arg==NULL) {
+		in->name = owstrdup(":") ;
+	} else {
+		in->name = owstrdup(arg);
+	}
+	in->busmode = bus_i2c;
+	return 0;
+	#else							/* OW_I2C */
+	LEVEL_DEFAULT("I2C (smbus DS2482-X00) support (intentionally) not included in compilation. Reconfigure and recompile.\n");
+	return 1;
+	#endif							/* OW_I2C */
+}
+
+int ARG_Link(const char *arg)
+{
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
+	}
+	in->name = owstrdup(arg);
+	in->busmode = (arg[0] == '/') ? bus_link : bus_elink;
 	return 0;
 }
 
@@ -104,14 +160,54 @@ int ARG_Mock(const char *arg)
 	return 0;
 }
 
-int ARG_Tester(const char *arg)
+int ARG_Net(const char *arg)
 {
 	struct connection_in *in = NewIn(NULL);
 	if (in == NULL) {
 		return 1;
 	}
 	in->name = owstrdup(arg);
-	in->busmode = bus_tester;
+	in->busmode = bus_server;
+	return 0;
+}
+
+int ARG_Parallel(const char *arg)
+{
+	#if OW_PARPORT
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
+	}
+	in->name = owstrdup(arg);
+	in->busmode = bus_parallel;
+	return 0;
+	#else							/* OW_PARPORT */
+	LEVEL_DEFAULT("Parallel port support (intentionally) not included in compilation. For DS1410E. That's ok, it doesn't work anyways.\n");
+	return 1;
+	#endif							/* OW_PARPORT */
+}
+
+int ARG_Passive(char *adapter_type_name, const char *arg)
+{
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
+	}
+	in->name = owstrdup(arg);
+	in->busmode = bus_passive;
+	// special set name of adapter here
+	in->adapter_name = adapter_type_name;
+	return 0;
+}
+
+int ARG_Serial(const char *arg)
+{
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
+	}
+	in->name = owstrdup(arg);
+	in->busmode = bus_serial;
 	return 0;
 }
 
@@ -135,111 +231,20 @@ int ARG_Side(const char *arg)
 	return 0;
 }
 
-int ARG_Device(const char *arg)
-{
-	struct stat sbuf;
-	if (stat(arg, &sbuf)) {
-		LEVEL_DEFAULT("Cannot access device %s\n", arg);
-		return 1;
-	}
-	if (!S_ISCHR(sbuf.st_mode)) {
-		LEVEL_DEFAULT("Not a \"character\" device %s (st_mode=%x)\n", arg, sbuf.st_mode);
-		return 1;
-	}
-	if (major(sbuf.st_rdev) == 99) {
-		return ARG_Parallel(arg);
-	}
-	if (major(sbuf.st_rdev) == 89) {
-		return ARG_I2C(arg);
-	}
-	return ARG_Serial(arg);
-}
-
-int ARG_Serial(const char *arg)
+int ARG_Tester(const char *arg)
 {
 	struct connection_in *in = NewIn(NULL);
 	if (in == NULL) {
 		return 1;
 	}
 	in->name = owstrdup(arg);
-	in->busmode = bus_serial;
+	in->busmode = bus_tester;
 	return 0;
-}
-
-int ARG_Passive(char *adapter_type_name, const char *arg)
-{
-	struct connection_in *in = NewIn(NULL);
-	if (in == NULL) {
-		return 1;
-	}
-	in->name = owstrdup(arg);
-	in->busmode = bus_passive;
-	// special set name of adapter here
-	in->adapter_name = adapter_type_name;
-	return 0;
-}
-
-int ARG_Link(const char *arg)
-{
-    struct connection_in *in = NewIn(NULL);
-    if (in == NULL) {
-        return 1;
-    }
-    in->name = owstrdup(arg);
-    in->busmode = (arg[0] == '/') ? bus_link : bus_elink;
-    return 0;
-}
-
-int ARG_HA7E(const char *arg)
-{
-    struct connection_in *in = NewIn(NULL);
-    if (in == NULL) {
-        return 1;
-    }
-    in->name = owstrdup(arg);
-    in->busmode = bus_ha7e ;
-    return 0;
-}
-
-int ARG_Parallel(const char *arg)
-{
-#if OW_PARPORT
-	struct connection_in *in = NewIn(NULL);
-	if (in == NULL) {
-		return 1;
-	}
-	in->name = owstrdup(arg);
-	in->busmode = bus_parallel;
-	return 0;
-#else							/* OW_PARPORT */
-	LEVEL_DEFAULT("Parallel port support (intentionally) not included in compilation. For DS1410E. That's ok, it doesn't work anyways.\n");
-	return 1;
-#endif							/* OW_PARPORT */
-}
-
-int ARG_I2C(const char *arg)
-{
-#if OW_I2C
-	struct connection_in *in = NewIn(NULL);
-	if (in == NULL) {
-		return 1;
-	}
-	if ( arg==NULL) {
-		in->name = owstrdup(":") ;
-	} else {
-		in->name = owstrdup(arg);
-    	}
-	in->busmode = bus_i2c;
-	return 0;
-#else							/* OW_I2C */
-	LEVEL_DEFAULT("I2C (smbus DS2482-X00) support (intentionally) not included in compilation. Reconfigure and recompile.\n");
-	return 1;
-#endif							/* OW_I2C */
 }
 
 int ARG_USB(const char *arg)
 {
-#if OW_USB
+	#if OW_USB
 	struct connection_in *in = NewIn(NULL);
 	if (in == NULL) {
 		return 1;
@@ -276,24 +281,19 @@ int ARG_USB(const char *arg)
 		}
 	}
 	return 0;
-#else							/* OW_USB */
+	#else							/* OW_USB */
 	LEVEL_DEFAULT("USB support (intentionally) not included in compilation. Check LIBUSB, then reconfigure and recompile.\n");
 	return 1;
-#endif							/* OW_USB */
+	#endif							/* OW_USB */
 }
 
-int ARG_Generic(const char *arg)
+int ARG_Xport(const char *arg)
 {
-	if (arg && arg[0]) {
-		switch (arg[0]) {
-		case '/':
-			return ARG_Device(arg);
-		case 'u':
-		case 'U':
-			return ARG_USB(&arg[1]);
-		default:
-			return ARG_Net(arg);
-		}
+	struct connection_in *in = NewIn(NULL);
+	if (in == NULL) {
+		return 1;
 	}
-	return 1;
+	in->name = arg ? owstrdup(arg) : NULL;
+	in->busmode = bus_xport;
+	return 0;
 }
