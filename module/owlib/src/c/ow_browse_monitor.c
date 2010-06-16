02a01e006e2c931461d8983eccdef0e346e51dd2
Paul Alfille <paul.alfille@gmail.com>
2010-06-16 02:08:06 +0000

    Add w1_monitor and browse_monitor to source code.

---
+++ module/owlib/src/c/ow_browse_monitor.c
@@ -0,0 +1,71 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2003 Paul H Alfille
+    email: palfille@earthlink.net
+    Released under the GPL
+    See the header file: ow.h for full attribution
+    1wire/iButton system from Dallas Semiconductor
+*/
+
+#include <config.h>
+#include "owfs_config.h"
+#include "ow.h"
+#include "ow_connection.h"
+
+/* All the rest of the program sees is the BadAdapter_detect and the entry in iroutines */
+
+static void Browse_close(struct connection_in *in);
+static GOOD_OR_BAD browse_in_use(const struct connection_in * in_selected) ;
+
+/* Device-specific functions */
+GOOD_OR_BAD Browse_detect(struct connection_in *in)
+{
+	in->file_descriptor = FILE_DESCRIPTOR_BAD;
+	in->iroutines.detect = Browse_detect;
+	in->Adapter = adapter_browse_monitor;	/* OWFS assigned value */
+	in->iroutines.reset = NULL;
+	in->iroutines.next_both = NULL;
+	in->iroutines.PowerByte = NULL;
+	in->iroutines.ProgramPulse = NULL;
+	in->iroutines.sendback_data = NULL;
+	in->iroutines.sendback_bits = NULL;
+	in->iroutines.select = NULL;
+	in->iroutines.reconnect = NULL;
+	in->iroutines.close = Browse_close;
+	in->iroutines.flags = ADAP_FLAG_sham;
+	in->adapter_name = "ZeroConf monitor";
+	in->busmode = bus_browse ;
+	
+	RETURN_BAD_IF_BAD( browse_in_use(in) ) ;
+	
+	if (Globals.zero == zero_none ) {
+		LEVEL_DEFAULT("Zeroconf/Bonjour is disabled since Bonjour or Avahi library wasn't found.");
+		return gbBAD;
+	} else {
+		OW_Browse();
+	}
+	return gbGOOD ;
+}
+
+static GOOD_OR_BAD browse_in_use(const struct connection_in * in_selected)
+{
+	struct connection_in *in;
+
+	for (in = Inbound_Control.head; in != NULL; in = in->next) {
+		if ( in == in_selected ) {
+			continue ;
+		}
+		if ( in->busmode != bus_browse ) {
+			continue ;
+		}
+		return gbBAD ;
+	}
+	return gbGOOD;					// not found in the current inbound list
+}
+
+static void Browse_close(struct connection_in *in)
+{
+	(void) in;
+}
---
+++ module/owlib/src/c/ow_w1_monitor.c
@@ -0,0 +1,71 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2003 Paul H Alfille
+    email: palfille@earthlink.net
+    Released under the GPL
+    See the header file: ow.h for full attribution
+    1wire/iButton system from Dallas Semiconductor
+*/
+
+#include <config.h>
+#include "owfs_config.h"
+#include "ow.h"
+#include "ow_connection.h"
+
+/* All the rest of the program sees is the BadAdapter_detect and the entry in iroutines */
+
+static void W1_close(struct connection_in *in);
+static GOOD_OR_BAD w1_in_use(const struct connection_in * in_selected) ;
+
+/* Device-specific functions */
+GOOD_OR_BAD W1_monitor_detect(struct connection_in *in)
+{
+	in->file_descriptor = FILE_DESCRIPTOR_BAD;
+	in->iroutines.detect = W1_detect;
+	in->Adapter = adapter_w1_monitor;	/* OWFS assigned value */
+	in->iroutines.reset = NULL;
+	in->iroutines.next_both = NULL;
+	in->iroutines.PowerByte = NULL;
+	in->iroutines.ProgramPulse = NULL;
+	in->iroutines.sendback_data = NULL;
+	in->iroutines.sendback_bits = NULL;
+	in->iroutines.select = NULL;
+	in->iroutines.reconnect = NULL;
+	in->iroutines.close = W1_close;
+	in->iroutines.flags = ADAP_FLAG_sham;
+	in->adapter_name = "W1 monitor";
+	in->busmode = bus_w1_monitor ;
+	
+	RETURN_BAD_IF_BAD( w1_in_use(in) ) ;
+	
+#if OW_W1
+	W1_Browse() ;
+	return gbGOOD ;
+#else /* OW_W1 */
+	LEVEL_DEFAULT("W1 (the linux kernel 1-wire system) is not supported");
+	return gbBAD ;
+#endif /* OW_W1 */
+}
+
+static GOOD_OR_BAD w1_in_use(const struct connection_in * in_selected)
+{
+	struct connection_in *in;
+
+	for (in = Inbound_Control.head; in != NULL; in = in->next) {
+		if ( in == in_selected ) {
+			continue ;
+		}
+		if ( in->busmode != bus_w1_monitor ) {
+			continue ;
+		}
+		return gbBAD ;
+	}
+	return gbGOOD;					// not found in the current inbound list
+}
+
+static void W1_close(struct connection_in *in)
+{
+	(void) in;
+}
