7995737208c142b710140deeffd2e978a7ae59f0
Paul Alfille <paul.alfille@gmail.com>
2010-11-29 04:04:20 +0000

    Start of DS28E10 support

---
+++ module/owlib/src/include/ow_2810.h
@@ -0,0 +1,24 @@
+/*
+$Id$
+    OWFS -- One-Wire filesystem
+    OWHTTPD -- One-Wire Web Server
+    Written 2003 Paul H Alfille
+	email: palfille@earthlink.net
+	Released under the GPL
+	See the header file: ow.h for full attribution
+	1wire/iButton system from Dallas Semiconductor
+*/
+
+#ifndef OW_2810_H
+#define OW_2810_H
+
+#ifndef OWFS_CONFIG_H
+#error Please make sure owfs_config.h is included *before* this header file
+#endif
+#include "ow_standard.h"
+
+/* ------- Structures ----------- */
+
+DeviceHeader(DS28E10);
+
+#endif							/* OW_2810_H */
