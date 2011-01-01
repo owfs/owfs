955350e830837d12a6e5300102d768e9773177bd
Paul Alfille <paul.alfille@gmail.com>
2011-01-01 15:02:44 +0000

    Take out some temporary debugging messages.

--- module/owlib/src/c/ow_tcp_read.c
+++ module/owlib/src/c/ow_tcp_read.c
@@ -127,17 +127,14 @@ void tcp_read_flush( FILE_DESCRIPTOR_OR_ERROR file_descriptor)
 		return;
 	}
 
-	LEVEL_DEBUG("tcp flush post get FD=%d",file_descriptor);
-
 	if (fcntl(file_descriptor, F_SETFL, flags | O_NONBLOCK) < 0) {
  		return;
 	}
-	LEVEL_DEBUG("tcp flush post_set FD=%d",file_descriptor);
+
 	while ((nread = read(file_descriptor,  buffer, 16)) > 0) {
 		Debug_Bytes("tcp_read_flush", buffer, nread);
 		continue;
 	}
 
 	fcntl(file_descriptor, F_SETFL, flags);
-	LEVEL_DEBUG("Done with flush");
 }
