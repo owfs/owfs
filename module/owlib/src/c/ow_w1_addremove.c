ac4250978a89597cfae654e5779412fa6a49f0bb
Paul Alfille <paul.alfille@gmail.com>
2011-12-03 21:52:09 +0000

    Fix W1 bus removal

--- module/owlib/src/c/ow_w1_addremove.c
+++ module/owlib/src/c/ow_w1_addremove.c
@@ -17,10 +17,11 @@ $Id$
 #include "ow_w1.h"
 #include "ow_connection.h"
 
-static struct connection_in * CreateIn(int bus_master) ;
+static struct connection_in * CreateW1In(int bus_master) ;
 static GOOD_OR_BAD W1_nomatch( struct connection_in * trial, struct connection_in * existing ) ;
 
-static struct connection_in * CreateIn(int bus_master)
+// Create a new bus master (W1 type)
+static struct connection_in * CreateW1In(int bus_master)
 {
 	struct connection_in * in = AllocIn(NO_CONNECTION) ;
 	char name[63] ;
@@ -40,6 +41,7 @@ static struct connection_in * CreateIn(int bus_master)
 
 	SOC(in)->devicename = owstrdup(name) ;
 	in->master.w1.id = bus_master ;
+	in->busmode = bus_w1 ;
 	in->master.w1.w1_slave_order = w1_slave_order_unknown ;
 	Init_Pipe( in->master.w1.netlink_pipe ) ;
 	if ( BAD( W1_detect(in)) ) {
@@ -47,7 +49,7 @@ static struct connection_in * CreateIn(int bus_master)
 		return NO_CONNECTION ;
 	}	
 		 
-	LEVEL_DEBUG("Created a new bus %s",SOC(in)->devicename) ;
+	LEVEL_DEBUG("Setup structure for %s",SOC(in)->devicename) ;
 	return in ;
 }
 
@@ -65,20 +67,22 @@ static GOOD_OR_BAD W1_nomatch( struct connection_in * trial, struct connection_i
 
 void AddW1Bus( int bus_master )
 {
-	struct connection_in * new_in = CreateIn(bus_master) ;
+	struct connection_in * new_in = CreateW1In(bus_master) ;
 	
 	if ( new_in == NO_CONNECTION ) {
 		return ;
 	}
 
-	LEVEL_DEBUG("Request master be added: w1_master%d.", bus_master);
+	LEVEL_DEBUG("Request master be added: w1_bus_master%d.", bus_master);
 	SOC(new_in)->type = ct_none ;
 	Add_InFlight( W1_nomatch, new_in ) ;
 }
 
 void RemoveW1Bus( int bus_master )
 {
-	Del_InFlight( W1_nomatch, CreateIn(bus_master) ) ;
+	struct connection_in * in = CreateW1In( bus_master ) ;
+	
+	Del_InFlight( W1_nomatch, in ) ; // tolerant of in==NO_CONNECTION
 }
 
 #endif /* OW_W1 && OW_MT */
