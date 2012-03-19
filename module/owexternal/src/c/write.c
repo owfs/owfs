ede058deed6c5f04f331c0a2c311905e47bab66f
Paul Alfille <paul.alfille@gmail.com>
2012-03-19 20:19:15 +0000

    Add owexternal

---
+++ module/owexternal/src/c/write.c
@@ -0,0 +1,65 @@
+/*
+$Id$
+    OW_HTML -- OWFS used for the web
+    OW -- One-Wire filesystem
+
+    Written 2004 Paul H Alfille
+
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* owserver -- responds to requests over a network socket, and processes them on the 1-wire bus/
+         Basic idea: control the 1-wire bus and answer queries over a network socket
+         Clients can be owperl, owfs, owhttpd, etc...
+         Clients can be local or remote
+                 Eventually will also allow bounce servers.
+
+         syntax:
+                 owserver
+                 -u (usb)
+                 -d /dev/ttyS1 (serial)
+                 -p tcp port
+                 e.g. 3001 or 10.183.180.101:3001 or /tmp/1wire
+*/
+
+#include "owexternal.h"
+
+/* Write, called from Handler with the following caveates: */
+/* path is path, already parsed, and null terminated */
+/* sm has been read, cm has been zeroed */
+/* pn is configured */
+/* data is what will be written, of sm->size length */
+/* cm fully constructed */
+/* cm.ret is also set to an error <0 or the written length */
+void WriteHandler(struct handlerdata *hd, struct client_msg *cm, struct one_wire_query *owq)
+{
+	int ret;
+
+	LEVEL_DEBUG("WriteHandler: hd->sm.payload=%d hd->sm.size=%d hd->sm.offset=%d OWQ_size=%d OWQ_offset=%d", hd->sm.payload, hd->sm.size, hd->sm.offset, OWQ_size(owq), OWQ_offset(owq));
+	ret = FS_write_postparse(owq);
+
+	//printf("Handler: WRITE done\n");
+	if (ret < 0) {
+		cm->size = 0;
+		cm->ret = ret;
+	} else {
+		cm->size = ret;
+		cm->ret = 0;
+		cm->control_flags = OWQ_pn(owq).control_flags;
+		if (hd->persistent)
+			cm->control_flags |= PERSISTENT_MASK;
+	}
+}
---
+++ module/owexternal/src/include/owserver.h
@@ -0,0 +1,87 @@
+/*
+$Id$
+   OWFS and OWHTTPD
+   one-wire file system and
+   one-wire web server
+
+    By Paul H Alfille
+    {c} 2003 GPL
+    palfille@earthlink.net
+*/
+
+/* OWSERVER - specific header */
+
+#ifndef OWSERVER_H
+#define OWSERVER_H
+
+#include <config.h>
+#include "owfs_config.h"
+#include "ow.h"
+#include "ow_connection.h"
+
+pthread_mutex_t persistence_mutex ;
+#define PERSISTENCELOCK    _MUTEX_LOCK(   persistence_mutex ) ;
+#define PERSISTENCEUNLOCK  _MUTEX_UNLOCK( persistence_mutex ) ;
+
+#define TOCLIENTLOCK(hd) _MUTEX_LOCK( (hd)->to_client )
+#define TOCLIENTUNLOCK(hd) _MUTEX_UNLOCK( (hd)->to_client )
+
+enum toclient_state {
+	toclient_postping , // also initial state
+	toclient_postmessage, // only for interme4diate messages like DIR entries
+	toclient_complete, // final payload has been sent
+} ;
+
+// this structure holds the data needed for the handler function called in a separate thread by the ping wrapper
+struct handlerdata {
+	int file_descriptor;
+	int persistent;
+	pthread_mutex_t to_client;
+	int ping_pipe[2] ;
+	enum toclient_state toclient ;
+	struct timeval tv;
+	struct server_msg sm;
+	struct serverpackage sp;
+};
+
+/* read from client, free return pointer if not Null */
+int FromClient(struct handlerdata *hd);
+
+/* Send fully configured message back to client */
+int ToClient(int file_descriptor, struct client_msg *cm, char *data);
+
+/* Read from 1-wire bus and return file contents */
+void *ReadHandler(struct handlerdata *hd, struct client_msg *cm, struct one_wire_query *owq);
+
+/* write a new value ot a 1-wire device */
+void WriteHandler(struct handlerdata *hd, struct client_msg *cm, struct one_wire_query *owq);
+
+/* Clasic directory -- one value at a time */
+void DirHandler(struct handlerdata *hd, struct client_msg *cm, const struct parsedname *pn);
+
+/* Newer directory-at-once */
+void *DirallHandler(struct handlerdata *hd, struct client_msg *cm, const struct parsedname *pn);
+
+/* Newer directory-at-once with directory '/' */
+void *DirallslashHandler(struct handlerdata *hd, struct client_msg *cm, const struct parsedname *pn);
+
+/* Handle the actual request -- pings handled higher up */
+void *DataHandler(void *v);
+
+/* Handle a client request, including timeout pings */
+void Handler(int file_descriptor);
+
+/* Send a response to client of an error */
+void ErrorToClient(struct handlerdata *hd, struct client_msg * cm ) ;
+
+#if OW_MT
+
+/* Send a timeout ping */
+void PingClient(struct handlerdata *hd);
+
+/* Loop waiting for finish sending pings */
+void PingLoop(struct handlerdata *hd) ;
+
+#endif /* OW_MT */
+
+#endif							/* OWSERVER_H */
