158adec26ab116e506653cf540359ed6e3f83fdf
Paul Alfille <paul.alfille@gmail.com>
2014-02-26 01:26:17 +0000

    Updated powerbyte and power bit to release for native power routines as well.

--- module/owlib/src/c/ow_powerbit.c
+++ module/owlib/src/c/ow_powerbit.c
@@ -29,35 +29,54 @@ GOOD_OR_BAD BUS_PowerBit(BYTE data, BYTE * resp, UINT delay, const struct parsed
 	GOOD_OR_BAD ret;
 	struct connection_in * in = pn->selected_connection ;
 
+	/* Special handling for some adapters that can handle independent channels
+	 * but others, like the DS2482-800 aren't completely independent
+	 * */
+	 
+	/* Very tricky -- we will release the port but not the channel 
+	 * so other threads can use the port's other channels
+	 * but we have to be careful of deadlocks
+	 * by fully releasing before relocking.
+	 * We are locking and releasing out of sequence
+	 * */
+
+	/* At the end, we need to relock because that's what the surrounding code expects 
+	 * -- that this routine is called with locked port and channel 
+	 * and leaves with same
+	 * */
+
 	if ( in->iroutines.PowerBit !=NO_POWERBIT_ROUTINE ) {
-		ret = (in->iroutines.PowerBit) (data, resp, delay, pn);
-	} else if (in->iroutines.flags & ADAP_FLAG_unlock_during_delay) {
-		/* Very tricky -- we will release the port but not the channel 
-		 * so other threads can use the port's other channels
-		 * but we have to be careful of deadlocks
-		 * by fully releasing before relocking.
-		 * We are locking and releasing out of sequence
-		 * */
-		// No real "power" in the default case
-		ret = BUS_sendback_bits(&data, resp, 1, pn);
-		// delay
-		PORTUNLOCKIN(in);
-		// we no longer have exclusive control of the channel
-		UT_delay(delay);
-		CHANNELUNLOCKIN(in);
-		// Could sneak into this channel
-		BUSLOCKIN(in);
-		// Wait for other thread to complete
-		/* we need to relock because that's what the surrounding code expects 
-		 * -- that this routine is called with locked port and channel 
-		 * and leaves with same
-		 * */
-	} else {
-		// send the packet
-		// No real "power" in the default case
-		ret = BUS_sendback_bits(&data, resp, 1, pn);
-		// delay
-		UT_delay(delay);
+		// use available bit level routine
+		if (in->iroutines.flags & ADAP_FLAG_unlock_during_delay) {
+			/* Can unlock port (but not channel) */
+			PORTUNLOCKIN(in);
+			ret = (in->iroutines.PowerBit) (data, resp, delay, pn);
+			CHANNELUNLOCKIN(in); // have to release channel, too
+			// now need to relock for further work in transaction
+			BUSLOCKIN(in);
+		} else {
+			// adapter doesn't support other channels during power		
+			ret = (in->iroutines.PowerBit) (data, resp, delay, pn);
+		}
+	} else { // send a bit and delay (use normal pull-up for power)
+		if (in->iroutines.flags & ADAP_FLAG_unlock_during_delay) {
+			ret = BUS_sendback_bits(&data, resp, 1, pn);
+			/* Can unlock port (but not channel) */
+			PORTUNLOCKIN(in);
+			// other channels are accessible, but this channel is still locked
+			// delay
+			UT_delay(delay);
+			CHANNELUNLOCKIN(in); // have to release channel, too
+			// now need to relock for further work in transaction
+			BUSLOCKIN(in);
+			// Wait for other thread to complete
+		} else {
+			// send the packet but keep port locked
+			// No real "power" in the default case
+			ret = BUS_sendback_bits(&data, resp, 1, pn);
+			// delay
+			UT_delay(delay);
+		}
 	}
 	if ( BAD(ret) ) {
 		STAT_ADD1_BUS(e_bus_pullup_errors, in);
--- module/owlib/src/c/ow_powerbyte.c
+++ module/owlib/src/c/ow_powerbyte.c
@@ -24,50 +24,68 @@ GOOD_OR_BAD BUS_PowerByte(BYTE data, BYTE * resp, UINT delay, const struct parse
 	GOOD_OR_BAD ret;
 	struct connection_in * in = pn->selected_connection ;
 
-	if ( in->iroutines.PowerByte !=NO_POWERBYTE_ROUTINE ) {
+	/* Special handling for some adapters that can handle independent channels
+	 * but others, like the DS2482-800 aren't completely independent
+	 * */
+	 
+	/* Very tricky -- we will release the port but not the channel 
+	 * so other threads can use the port's other channels
+	 * but we have to be careful of deadlocks
+	 * by fully releasing before relocking.
+	 * We are locking and releasing out of sequence
+	 * */
+
+	/* At the end, we need to relock because that's what the surrounding code expects 
+	 * -- that this routine is called with locked port and channel 
+	 * and leaves with same
+	 * */
+
+	if ( in->iroutines.PowerByte != NO_POWERBYTE_ROUTINE ) {
 		// use available byte level routine
-		ret = (in->iroutines.PowerByte) (data, resp, delay, pn);
+		if (in->iroutines.flags & ADAP_FLAG_unlock_during_delay) {
+			/* Can unlock port (but not channel) */
+			PORTUNLOCKIN(in);
+			// other channels are accessible, but this channel is still locked
+			ret = (in->iroutines.PowerByte) (data, resp, delay, pn);
+			CHANNELUNLOCKIN(in); // have to release channel, too
+			// now need to relock for further work in transaction
+			BUSLOCKIN(in);
+		} else {
+			// adapter doesn't support other channels during power		
+			ret = (in->iroutines.PowerByte) (data, resp, delay, pn);
+		}
 	} else if ( in->iroutines.PowerBit != NO_POWERBIT_ROUTINE && in->iroutines.sendback_bits != NO_SENDBACKBITS_ROUTINE ) {
-		// use available bit level routines
+		// use available bit level routine
 		BYTE sending[8];
 		BYTE receive[8] ;
 		int i ;
-		for ( i = 0 ; i < 8 ; ++i ) {
+		for ( i = 0 ; i < 8 ; ++i ) { // load up bits
 			sending[i] = UT_getbit(&data,i) ? 0xFF : 0x00 ;
 		}
-		ret = BUS_sendback_bits( sending, receive, 7, pn ) ;
+		ret = BUS_sendback_bits( sending, receive, 7, pn ) ; // send first 7 bits
 		if ( GOOD(ret) ) {
-			ret = BUS_PowerBit( sending[7], &receive[7], delay, pn ) ;
+			ret = BUS_PowerBit( sending[7], &receive[7], delay, pn ) ; // last bit with power and delay
 		}
 		for ( i = 0 ; i < 8 ; ++i ) {
 			UT_setbit( resp, i, receive[i] ) ;
 		}
-	} else if (in->iroutines.flags & ADAP_FLAG_unlock_during_delay) {
-		/* Very tricky -- we will release the port but not the channel 
-		 * so other threads can use the port's other channels
-		 * but we have to be careful of deadlocks
-		 * by fully releasing before relocking.
-		 * We are locking and releasing out of sequence
-		 * */
-		// send with no true power applied
-		ret = BUS_sendback_data(&data, resp, 1, pn);
-		// delay
-		PORTUNLOCKIN(in);
-		// we no longer have exclusive control of the channel
-		UT_delay(delay);
-		CHANNELUNLOCKIN(in);
-		// Could sneak into this channel
-		BUSLOCKIN(in);
-		// Wait for other thread to complete
-		/* we need to relock because that's what the surrounding code expects 
-		 * -- that this routine is called with locked port and channel 
-		 * and leaves with same
-		 * */
-	} else {
-		// send with no true power applied
-		ret = BUS_sendback_data(&data, resp, 1, pn);
-		// delay
-		UT_delay(delay);
+	} else { // send with no true power applied
+		if (in->iroutines.flags & ADAP_FLAG_unlock_during_delay) {
+			/* Can unlock port (but not channel) */
+			ret = BUS_sendback_data(&data, resp, 1, pn);
+			PORTUNLOCKIN(in);
+			// other channels are accessible, but this channel is still locked
+			// delay
+			UT_delay(delay);
+			CHANNELUNLOCKIN(in); // have to release channel, too
+			// now need to relock for further work in transaction
+			BUSLOCKIN(in);
+		} else {
+			// send with no true power applied
+			ret = BUS_sendback_data(&data, resp, 1, pn);
+			// delay
+			UT_delay(delay);
+		}
 	}
 	if ( BAD(ret) ) {
 		STAT_ADD1_BUS(e_bus_pullup_errors, in);
