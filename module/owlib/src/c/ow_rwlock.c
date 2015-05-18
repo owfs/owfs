289eaf2329b8cc8b80aa85c6e8572bd6be124629
Johan Ström <johan@stromnet.se>
2015-05-18 20:03:19 +0200

    Fix build issue when building without EXTENDED_RWLOCK_DEBUG

--- module/owlib/src/c/ow_rwlock.c
+++ module/owlib/src/c/ow_rwlock.c
@@ -25,7 +25,6 @@ void my_rwlock_init(my_rwlock_t * rwlock)
 	int semrc;
 	semrc = pthread_rwlock_init(rwlock, NULL);
 	if(semrc != 0) {
-		if ( 
 		LOCK_DEBUG("semrc=%d [%s] RWLOCK INIT", semrc, strerror(errno));
 		debug_crash();
 	}
