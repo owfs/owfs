owfs/owfs

commit e855721f4e02fd93b5345ba8e89462043973ba28

Fix reading and writing external sensors connected to an owserver

module/owlib/src/c/ow_read.c
@@ -68,11 +68,6 @@ SIZE_OR_ERROR FS_read_postparse(struct one_wire_query *owq)
 	struct parsedname *pn = PN(owq);
 	SIZE_OR_ERROR read_or_error;
 
-	// ServerRead jumps in here, perhaps with non-file entry
-	if (pn->selected_device == NO_DEVICE || pn->selected_filetype == NO_FILETYPE) {
-		return -EISDIR;
-	}
-
 	/* Normal read. Try three times */
 	LEVEL_DEBUG("%s", pn->path);
 	STATLOCK;
@@ -83,7 +78,12 @@ SIZE_OR_ERROR FS_read_postparse(struct one_wire_query *owq)
 	/* First try */
 	STAT_ADD1(read_tries[0]);
 
-	read_or_error = (pn->type == ePN_real) ? FS_read_real(owq) : FS_r_virtual(owq);
+	// ServerRead jumps in here, perhaps with non-file entry
+	if (pn->selected_device == NO_DEVICE || pn->selected_filetype == NO_FILETYPE) {
+		read_or_error = FS_r_given_bus(owq);
+	} else {
+		read_or_error = (pn->type == ePN_real) ? FS_read_real(owq) : FS_r_virtual(owq);
+	}
 
 	STATLOCK;
 	if (read_or_error >= 0) {

module/owlib/src/c/ow_get.c
@@ -38,13 +38,15 @@ static void getdircallback( void * v, const struct parsedname * const pn_entry )
 	}
 }
 
-static void getdir( struct charblob * cb, struct one_wire_query * owq ) 
-{  
-	if ( FS_dir( getdircallback, cb, PN(owq) ) != 0 ) {
+static int getdir( struct charblob * cb, struct one_wire_query * owq ) 
+{
+	int ret = 0;
+	if ( (ret = FS_dir( getdircallback, cb, PN(owq) )) != 0 ) {
 		CharblobClear( cb ) ;
 	} else if ( CharblobLength(cb) == 0 ) {
 		CharblobAddChar( '\0', cb) ;
 	}
+	return ret;
 }
 
 static char * copy_buffer( char * data, int size )
@@ -83,19 +85,42 @@ SIZE_OR_ERROR FS_get(const char *path, char **return_buffer, size_t * buffer_len
 		return -ENOENT;
 	}
 
-	if ( IsDir( PN(owq) ) ) { /* A directory of some kind */
+	// Check for known type.
+	if ( (PN(owq)->selected_filetype) != NO_FILETYPE ) { 
+		// local owlib knows the type. 
+		if ( IsDir( PN(owq) ) ) { /* A directory of some kind */
+			struct charblob cb ;
+			CharblobInit(&cb) ;
+			getdir( &cb, owq ) ;
+			size = CharblobLength(&cb) ;
+			*return_buffer = copy_buffer( CharblobData(&cb), size ) ;
+			CharblobClear(&cb) ;
+		} else { /* A regular file  -- so read */
+			if ( GOOD(OWQ_allocate_read_buffer(owq)) ) { // make the space in the buffer
+				size = FS_read_postparse(owq) ;
+				*return_buffer = copy_buffer( OWQ_buffer(owq), size ) ;
+			}
+		}
+	} else {
+		// local owlib doesn't know the type.
 		struct charblob cb ;
 		CharblobInit(&cb) ;
-		getdir( &cb, owq ) ;
-		size = CharblobLength(&cb) ;
-		*return_buffer = copy_buffer( CharblobData(&cb), size ) ;
-		CharblobClear( &cb ) ;
-	} else { /* A regular file  -- so read */
-		if ( GOOD(OWQ_allocate_read_buffer(owq)) ) { // make the space in the buffer
-			size = FS_read_postparse(owq) ;
-			*return_buffer = copy_buffer( OWQ_buffer(owq), size ) ;
+
+		// Try directory first.
+		if (getdir( &cb, owq ) != -ENOTDIR) {
+			// Is a directory.
+			size = CharblobLength(&cb) ;
+			*return_buffer = copy_buffer( CharblobData(&cb), size ) ;
+		} else {
+			// Is not a directory. Try file.
+			if ( GOOD(OWQ_allocate_read_buffer(owq)) ) { // make the space in the buffer
+				size = FS_read_postparse(owq) ;
+				*return_buffer = copy_buffer( OWQ_buffer(owq), size ) ;
+			}
 		}
-	}
+		CharblobClear(&cb) ;
+	}	
+
 	// the buffer is allocated by getdir or getval
 	OWQ_destroy(owq);
 

module/owlib/src/c/ow_parseinput.c
@@ -80,7 +80,7 @@ ZERO_OR_ERROR OWQ_parse_input(struct one_wire_query *owq)
 	default:
 		// Sort out locally unknown filetype.
 		if (OWQ_pn(owq).selected_filetype == NO_FILETYPE) {
-			return -ENOENT;
+			return FS_input_ascii(owq);
 		}
 
 		// Switch by known filetype.

module/owlib/src/c/ow_parsename.c
@@ -751,8 +751,8 @@ static enum parse_enum Parse_Property(char *filename, struct parsedname *pn)
 
 	// Special case for remote device. Use distant data
 	if ( pdev == &RemoteDevice ) {
-		// remote device, no known sn, can't handle a property
-		return parse_error ;
+		// remote device, no known sn, handle property in server.
+		return parse_done ;
 	}
 
 	// separate filename.dot

module/owlib/src/c/ow_write.c
@@ -91,7 +91,7 @@ SIZE_OR_ERROR FS_write_postparse(struct one_wire_query *owq)
 		return -EROFS;			// read-only invokation
 	}
 
-	if (IsDir(pn)) {
+	if (IsDir(pn) && !(BusIsServer(pn->selected_connection))) {
 		LEVEL_DEBUG("Attempt to write to a directory.");
 		return -EISDIR;			// not a file
 	}

commit cad4fde8e49ad87285a6d3f84c01bc06bfa15978

Fix a bug introduced in commit e855721f4e02fd93b5345ba8e89462043973ba28 causing owserver to crash when doing an explicit read on a locally known directory.
Thanks to Stephano Miccoli <mocme@icloud.com> for discovering that.

module/owlib/src/c/ow_read.c
@@ -78,10 +78,17 @@ SIZE_OR_ERROR FS_read_postparse(struct one_wire_query *owq)
 	/* First try */
 	STAT_ADD1(read_tries[0]);
 
-	// ServerRead jumps in here, perhaps with non-file entry
+	/* Check file type. */
 	if (pn->selected_device == NO_DEVICE || pn->selected_filetype == NO_FILETYPE) {
-		read_or_error = FS_r_given_bus(owq);
+		if (KnownBus(pn) && BusIsServer(pn->selected_connection)) {
+			/* Pass unknown remote filetype to remote owserver. */
+			read_or_error = FS_r_given_bus(owq);
+		} else {
+			/* Local unknown filetypes are directories. */
+			return -EISDIR;
+		}	
 	} else {
+		/* Local known filetypes are handled here. */
 		read_or_error = (pn->type == ePN_real) ? FS_read_real(owq) : FS_r_virtual(owq);
 	}
 