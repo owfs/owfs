owfs/owfs

commit c44db863a324bff4608639981ca05f73b43ab275

fixed false positives found by -Wimplicit-fallthrough

module/owlib/src/c/ow_server_message.c
@@ -579,8 +579,10 @@ static GOOD_OR_BAD To_Server( struct server_connection_state * scs, struct serve
 				break ;
 			}
 			// real error, fall through to close connection case
+			__attribute__ ((fallthrough));
 		case -2:
 			// fnctl error, fall through again
+			__attribute__ ((fallthrough));
 		case 0:
 			LEVEL_DEBUG("Server connection was closed.  Reconnecting.");
 			Close_Persistent( scs);

module/owlib/src/c/ow_1921.c
@@ -787,10 +787,13 @@ static ZERO_OR_ERROR FS_w_atrig(struct one_wire_query *owq)
 	switch (OWQ_U(owq)) {		/* Intentional fall-throughs in cases */
 	case 1:
 		data[0] |= 0x80;
+		__attribute__ ((fallthrough));
 	case 2:
 		data[1] |= 0x80;
+		__attribute__ ((fallthrough));
 	case 3:
 		data[2] |= 0x80;
+		__attribute__ ((fallthrough));
 	case 4:
 		data[3] |= 0x80;
 	}

module/owlib/src/c/ow_elabnet.c
@@ -500,6 +500,7 @@ static GOOD_OR_BAD PBM_directory(struct device_search *ds, struct connection_in 
 		case 'E':
 			LEVEL_DEBUG("PBM returned E: No devices in alarm");
 			// pass through
+			__attribute__ ((fallthrough));
 		case 'N':
 			// remove extra 2 bytes
 			LEVEL_DEBUG("PBM returned E or N: Empty bus");

module/owlib/src/c/ow_link.c
@@ -749,6 +749,7 @@ static GOOD_OR_BAD LINK_directory(struct device_search *ds, struct connection_in
 		case 'E':
 			LEVEL_DEBUG("LINK returned E: No devices in alarm");
 			// pass through
+			__attribute__ ((fallthrough));
 		case 'N':
 			// remove extra 2 bytes
 			LEVEL_DEBUG("LINK returned E or N: Empty bus");

module/owlib/src/c/owlib.c
@@ -124,7 +124,7 @@ static GOOD_OR_BAD SetupSingleInboundConnection( struct port_in * pin )
 		}
 		// Fall Through
 		in->adapter_name = "DS9097";	// need to set adapter name for this approach to passive adapter
-
+		__attribute__ ((fallthrough));
 	case bus_passive:
 		if ( BAD( DS9097_detect(pin) )) {
 			LEVEL_DEFAULT("Cannot detect DS9097 (passive) interface on %s.", DEVICENAME(in));

module/ownet/c/src/c/ow_server.c
@@ -426,6 +426,7 @@ static int To_Server( struct server_connection_state * scs, struct server_msg * 
 				break ;
 			}
 			// real error, fall through to close connection case
+			__attribute__ ((fallthrough));
 		case -2:
 			// fnctl error, fall through again
 		case 0:

module/owshell/src/c/globals.c
@@ -44,7 +44,7 @@ static void Cleanup(void)
 #endif
 }
 
-void Exit(int exit_code)
+__attribute__((noreturn)) void Exit(int exit_code)
 {
 	Cleanup();
 	exit(exit_code);

module/owshell/src/include/owshell.h
@@ -317,6 +317,6 @@ struct connection_in {
 extern struct connection_in s_owserver_connection;
 extern struct connection_in *owserver_connection;
 
-void Exit(int exit_code);
+__attribute__((noreturn)) void Exit(int exit_code);
 
 #endif							/* OWSHELL_H */

commit 5c396185752434b7f7b687e8c86c73ea888337b2

fixed a problem with gcc<7.1 and clang introduced by commit c44db863a324bff4608639981ca05f73b43ab275

module/owlib/src/c/ow_server_message.c
@@ -582,6 +582,7 @@ static GOOD_OR_BAD To_Server( struct server_connection_state * scs, struct serve
 			__attribute__ ((fallthrough));
 		case -2:
 			// fnctl error, fall through again
+			;
 			__attribute__ ((fallthrough));
 		case 0:
 			LEVEL_DEBUG("Server connection was closed.  Reconnecting.");