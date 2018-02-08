bbafda09721e248cd9205c3d1117230268dcf38a
echrstnn <estebanc@openmailbox.org>
2018-02-08 22:13:37 +0100

    Fixed OW_init() fail when parsing command line options before calling it

--- module/owlib/src/c/ow_opt.c
+++ module/owlib/src/c/ow_opt.c
@@ -578,6 +578,7 @@ GOOD_OR_BAD owopt_packed(const char *params)
 
 	// analyze argv/argc as if real command line arguments
 	ArgCopy( argc, argv ) ;
+	optind = 1;
 
 	while (ret == 0) {
 		if ((option_char = getopt_long(argc, argv, OWLIB_OPT, owopts_long, NULL)) == -1) {
