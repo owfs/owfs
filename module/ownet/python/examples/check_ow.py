fcc0047f0e064579a89507cc4c1903240ff6129d
Thomas Perale <thomas.perale@mind.be>
2024-03-11 12:44:34 +0100

    ownet/python: port examples to python3

--- module/ownet/python/examples/check_ow.py
+++ module/ownet/python/examples/check_ow.py
@@ -24,14 +24,14 @@ class nagios:
 # FIXME: need to add more help text in the init_string and sensor_path parameters
 parser = OptionParser(usage='usage: %prog [options] server port sensor_path',
                       version='%prog ' + ownet.__version__)
-parser.add_option('-v', dest='verbose', action='count',             help='multiple -v increases the level of debugging output.')
-parser.add_option('-w', dest='warning',                 type='int', help='warning level.')
-parser.add_option('-c', dest='critical',                type='int', help='critical level.')
+parser.add_option('-v', dest='verbose', action='count',             help='multiple -v increases the level of debugging output.', default=0)
+parser.add_option('-w', dest='warning',                 type='int', help='warning level.', default=0)
+parser.add_option('-c', dest='critical',                type='int', help='critical level.', default=0)
 parser.add_option('-f', dest='field',                               help='sensor field to be used for monitoring.', default='temperature')
 options, args = parser.parse_args()
 
 if len(args) != 3:
-    print 'OW ' + nagios.unknown[1] + ' - ' + 'missing command line arguments'
+    print('OW ' + nagios.unknown[1] + ' - ' + 'missing command line arguments')
     parser.print_help()
     sys.exit(nagios.unknown[0])
 
@@ -42,21 +42,21 @@ path   = args[2]
 try:
     s = ownet.Sensor(path, server=server, port=port)
 except ownet.exUnknownSensor as ex:
-    print 'OW ' + nagios.unknown[1] + ' - unknown sensor ' + str(ex)
+    print('OW ' + nagios.unknown[1] + ' - unknown sensor ' + str(ex))
     sys.exit(nagios.unknown[0])
 except socket.error as ex:
-    print 'OW ' + nagios.unknown[1] + ' - communication error ' + str(ex)
+    print('OW ' + nagios.unknown[1] + ' - communication error ' + str(ex))
     sys.exit(nagios.unknown[0])
 
 if options.verbose == 1:
-    print s
+    print(s)
 elif options.verbose > 1:
-    print s
-    print 'entryList:', s.entryList()
-    print 'sensorList:', s.sensorList()
+    print(s)
+    print('entryList:', s.entryList())
+    print('sensorList:', s.sensorList())
 
 if not hasattr(s, options.field):
-    print 'OW ' + nagios.unknown[1] + ' - ' + 'unknown field: ' + options.field
+    print('OW ' + nagios.unknown[1] + ' - ' + 'unknown field: ' + options.field)
     sys.exit(nagios.unknown[0])
 
 val = getattr(s, options.field)
@@ -71,5 +71,5 @@ else:
     status    = nagios.ok[1]
     exit_code = nagios.ok[0]
 
-print 'OW %s - %s %s: %i| %s/%s=%i' % (status, path, options.field, val, path, options.field, val)
+print('OW %s - %s %s: %i| %s/%s=%i' % (status, path, options.field, val, path, options.field, val))
 sys.exit(exit_code)
--- module/ownet/python/examples/temperatures.py
+++ module/ownet/python/examples/temperatures.py
@@ -4,16 +4,16 @@ import sys
 import ownet
 
 if len(sys.argv) != 3:
-    print 'temperatures.py server port'
+    print('temperatures.py server port')
     sys.exit(1)
 
 r = ownet.Sensor('/', server=sys.argv[1], port=int(sys.argv[2]))
 e = r.entryList()
 s = r.sensorList()
-print 'r:', r
-print 'r.entryList():', e
-print 'r.sensorList():', s
+print('r:', r)
+print('r.entryList():', e)
+print('r.sensorList():', s)
 
 for x in r.sensors():
     if hasattr(x, 'temperature'):
-        print x, x.temperature
+        print(x, x.temperature)
